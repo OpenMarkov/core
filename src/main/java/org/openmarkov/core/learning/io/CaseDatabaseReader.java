e419b8190f6d7963d34540c3f20bb4fe5e93ba8c
ibermejo <ibermejo@localhost>
2011-11-22 19:04:40 +0100

    Created the CaseDatabase class that consists of a ProbNet and a bidimensional array of ints Modified the interfaces of CaseDatabaseReader and CaseDatabaseWriter to return and receive an instance of CaseDatabase

--- src/main/java/org/openmarkov/core/learning/io/CaseDatabaseReader.java
+++ src/main/java/org/openmarkov/core/learning/io/CaseDatabaseReader.java
@@ -3,9 +3,8 @@ package org.openmarkov.core.learning.io;
 import java.io.IOException;
 
 import org.openmarkov.core.exception.InvalidStateException;
-import org.openmarkov.core.model.network.ProbNet;
 
 public interface CaseDatabaseReader
 {
-    public int[][] load(String filename, ProbNet probnet) throws IOException, InvalidStateException;
+    public CaseDatabase load(String filename) throws IOException, InvalidStateException;
 }
--- src/main/java/org/openmarkov/core/learning/io/CaseDatabaseWriter.java
+++ src/main/java/org/openmarkov/core/learning/io/CaseDatabaseWriter.java
@@ -2,9 +2,7 @@ package org.openmarkov.core.learning.io;
 
 import java.io.IOException;
 
-import org.openmarkov.core.model.network.ProbNet;
-
 public interface CaseDatabaseWriter
 {
-    public void save(String filename, ProbNet probnet, int[][] cases) throws IOException;
+    public void save(String filename, CaseDatabase database) throws IOException;
 }

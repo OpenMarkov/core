15c51f8a2be5585bb1aea9835c056aaea1ddb131
ibermejo <ibermejo@localhost>
2011-11-30 19:46:50 +0100

    Move mdp back to remaining

--- src/main/java/org/openmarkov/core/mdp/MDPEvaluationException.java
+++
@@ -1,9 +0,0 @@
-package org.openmarkov.core.mdp;
-
-public class MDPEvaluationException extends Exception {
-	private static final long serialVersionUID = 7120910527454847613L;
-	
-	public MDPEvaluationException(String string) {
-		super(string);
-	}
-}
--- src/main/java/org/openmarkov/core/mdp/MDPFiniteHorizonPolicy.java
+++
@@ -1,8 +0,0 @@
-package org.openmarkov.core.mdp;
-
-public abstract class MDPFiniteHorizonPolicy {
-
-	public abstract MDPPolicy getTimeSlicePolicy(int timeSlot);
-
-	public abstract void setTimeSlicePolicy(int timeSlot, MDPPolicy mdpPolicy);
-}

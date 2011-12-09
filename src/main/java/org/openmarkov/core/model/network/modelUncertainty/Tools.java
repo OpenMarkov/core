23dc7f6e5bd1de84e94abf571dd2289dd2de6a42
mluque <mluque@localhost>
2011-12-09 21:45:50 +0100

    Changes for inference in Bayesian networks and influence diagrams.

--- src/main/java/org/openmarkov/core/model/network/modelUncertainty/Tools.java
+++ src/main/java/org/openmarkov/core/model/network/modelUncertainty/Tools.java
@@ -3,8 +3,8 @@ package org.openmarkov.core.model.network.modelUncertainty;
 public class Tools {
 	   public static double sum(double[] aa){
            double sum=0.0D;
-           for(int i=0; i<aa.length; i++){
-                   sum+=aa[i];
+           for(int i = 0; i < aa.length; i++){
+                   sum = sum + aa[i];
            }
            return sum;
 	   }
@@ -35,4 +35,39 @@ public class Tools {
 	   public static double square(double x){
 		   return Math.pow(x, 2.0);
 	   }
+	   
+	   /**
+	 * @param x
+	 * @return An estimation of the mean of an array of real numbers sampled
+	 */
+	public double meanSample(double []x){
+		  double mu;
+	   	  int length = x.length;
+		  mu = (length>0)?(sum(x)/length):0.0;
+		  return mu;  
+	   }
+	   
+	/**
+	 * @param x
+	 * @return An estimation of the variance of an array of real numbers sampled. It
+	 *         uses the equation of the quasi-variance
+	 */
+	public double varianceSample(double[] x) {
+		double mu;
+		double sumSquares;
+		double variance;
+
+		mu = meanSample(x);
+		sumSquares = 0.0;
+		int length = x.length;
+		if (length > 1) {
+			for (int i = 0; i < length; i++) {
+				sumSquares = sumSquares + Math.pow((x[i] - mu), 2);
+			}
+			variance = sumSquares / (length - 1);
+		} else {
+			variance = 0.0;
+		}
+		return variance;
+	}
 }

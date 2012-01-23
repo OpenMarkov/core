c80e5ef2a10707fc761c8b19338983fd052002b8
maryebra <maryebra@localhost>
2012-01-23 15:15:57 +0100

    Initialize leak potential in ICIPotential

--- src/main/java/org/openmarkov/core/model/network/potential/canonical/ICIPotential.java
+++ src/main/java/org/openmarkov/core/model/network/potential/canonical/ICIPotential.java
@@ -46,33 +46,46 @@ public abstract class ICIPotential extends Potential {
 		this.family = modelType.getFamily();
 		subPotentials = new ArrayList<TablePotential>();
 		
-		for(int i = 1; i < variables.size (); ++i)
-		{
-            double [] probabilities = new double[variables.get(0).getNumStates () * variables.get(i).getNumStates ()];
-            for(int j = 0; j < variables.get(i).getNumStates (); ++j)
-            {
-                for(int k = 0; k < variables.get(0).getNumStates (); ++k)
-                {
-                    probabilities[j* variables.get(0).getNumStates() + k] = (k == j)? 1.0: 0.0; 
-                }
-            }
-            
-            ArrayList<Variable> linkVariables = new ArrayList<Variable>();
-            linkVariables.add(variables.get(0));
-            linkVariables.add(variables.get(i));
-            try
-            {
-                addSubPotential (new TablePotential(linkVariables, role));
-            }
-            catch (NotEnoughMemoryException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+		try {
+			for (int i = 1; i < variables.size(); ++i) {
+				double[] probabilities = new double[variables.get(0)
+						.getNumStates() * variables.get(i).getNumStates()];
+				for (int j = 0; j < variables.get(i).getNumStates(); ++j) {
+					for (int k = 0; k < variables.get(0).getNumStates(); ++k) {
+						probabilities[j * variables.get(0).getNumStates() + k] = (k == j) ? 1.0
+								: 0.0;
+					}
+				}
+
+				ArrayList<Variable> linkVariables = new ArrayList<Variable>();
+				linkVariables.add(variables.get(0));
+				linkVariables.add(variables.get(i));
+				addSubPotential(new TablePotential(linkVariables, role));
+			}
+
+			addSubPotential(getDefaultLeakyPotential());
+		} catch (NotEnoughMemoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
 
+	public TablePotential getDefaultLeakyPotential() throws NotEnoughMemoryException
+	{
+		ArrayList<Variable> leakyVariables = new ArrayList<Variable>();
+		leakyVariables.add(variables.get(0));
+		TablePotential tablePotential = new TablePotential(leakyVariables, PotentialRole.CONDITIONAL_PROBABILITY);
+		double[] leakyParameters = new double[variables.get(0).getNumStates()];
+		leakyParameters[0] = 1.0;
+		for(int i=1; i<leakyParameters.length; ++i)
+		{
+			leakyParameters[0] = 0.0;
+		}
+		tablePotential.values = leakyParameters;
+		return tablePotential;
+	}
+	
 	// Methods
 	public abstract TablePotential getCPT() throws NotEnoughMemoryException;
 

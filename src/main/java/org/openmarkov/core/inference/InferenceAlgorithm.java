94ec852844206be3864cf41b31b41699ecad5855
ibermejo <ibermejo@localhost>
2012-01-25 13:17:20 +0100

    InferenceManager implemented

--- src/main/java/org/openmarkov/core/inference/InferenceAlgorithm.java
+++ src/main/java/org/openmarkov/core/inference/InferenceAlgorithm.java
@@ -1,16 +1,12 @@
 /*
-* Copyright 2011 CISIAD, UNED, Spain
-*
-* Licensed under the European Union Public Licence, version 1.1 (EUPL)
-*
-* Unless required by applicable law, this code is distributed
-* on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
-*/
+ * Copyright 2011 CISIAD, UNED, Spain Licensed under the European Union Public
+ * Licence, version 1.1 (EUPL) Unless required by applicable law, this code is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
+ */
 
 package org.openmarkov.core.inference;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Hashtable;
 
@@ -28,168 +24,180 @@ import org.openmarkov.core.exception.WrongGraphStructureException;
 import org.openmarkov.core.model.network.EvidenceCase;
 import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.core.model.network.Variable;
-import org.openmarkov.core.model.network.constraint.PNConstraint;
 import org.openmarkov.core.model.network.potential.Potential;
-import org.openmarkov.core.model.network.type.NetworkType;
 
-public abstract class InferenceAlgorithm {
-	
-	protected EvidenceCase evidence;
-	
-	protected ArrayList<Potential> imposedPolicies;
+public abstract class InferenceAlgorithm
+{
+    protected EvidenceCase                evidence;
+    protected ArrayList<Potential>        imposedPolicies;
+    /** This is a copy of the <code>ProbNet</code> received. */
+    protected ProbNet                     probNet;
+    /** For undo/redo operations. */
+    protected PNESupport                  pNESupport;
+    /**
+     * <code>true</code> if the network is prepared for obtaining the marginal
+     * probabilities.
+     */
+    // protected boolean compiled;
+    /**
+     * Indicates if the Bayesian network or the Cooper Policy Network has been
+     * compiled. If it is true, then the posteriori probabilities and expected
+     * utilities associated to each utility node have been computed.
+     */
+    protected boolean                     hasBeenCompiled;
+    protected StrategyUtilities           utilityTables;
+    protected Hashtable<Variable, Double> expectedUtilities;
+    protected Double                      globalExpectedUtility;
 
-	/** This is a copy of the <code>ProbNet</code> received. */
-	protected ProbNet probNet;
-	
-	/** For undo/redo operations. */
-	protected PNESupport pNESupport;
-	
-	
-	
-		
-	/** <code>true</code> if the network is prepared for obtaining the marginal probabilities. */
-	//protected boolean compiled;
-	
-	/**
-	 * Indicates if the Bayesian network or the Cooper Policy Network has been compiled. If it is true, then the posteriori probabilities
-	 * and expected utilities associated to each utility node have been computed.
-	 */
-	protected boolean hasBeenCompiled;
-	
-	protected StrategyUtilities utilityTables;
-	
-	protected Hashtable<Variable,Double> expectedUtilities;
-	
-	protected Double globalExpectedUtility;
-	
-	/**
-	 * @param globalExpectedUtility the globalExpectedUtility to set
-	 */
-	public void setGlobalExpectedUtility(Double globalExpectedUtility) {
-		this.globalExpectedUtility = globalExpectedUtility;
-	}
+    /**
+     * @param globalExpectedUtility the globalExpectedUtility to set
+     */
+    public void setGlobalExpectedUtility (Double globalExpectedUtility)
+    {
+        this.globalExpectedUtility = globalExpectedUtility;
+    }
 
+    // Constructor
+    public InferenceAlgorithm (ProbNet probNet)
+        throws NotEvaluableNetworkException
+    {
+        this.probNet = probNet;
+        evidence = new EvidenceCase ();
+        if (!isEvaluable (probNet))
+        {
+            throw new NotEvaluableNetworkException (probNet.toString ());
+        }
+    }
 
-	
+    public abstract boolean isEvaluable (ProbNet probNet);
 
+    /**
+     * @return the evidence
+     */
+    public EvidenceCase getEvidence ()
+    {
+        return evidence;
+    }
 
-		// Constructor
-		public InferenceAlgorithm(ProbNet probNet) 
-		throws NotEvaluableNetworkException {
-								
-			this.probNet = probNet;
-			evidence = new EvidenceCase();
-							
-			if (!isEvaluable(probNet)){
-				throw new NotEvaluableNetworkException(probNet.toString());
-			}
-		}
-			
-			
+    /**
+     * @param evidence the evidence to set
+     */
+    public void setEvidence (EvidenceCase evidence)
+    {
+        this.evidence = evidence;
+    }
 
+    /**
+     * @return the imposedPolicies
+     */
+    public ArrayList<Potential> getImposedPolicies ()
+    {
+        return imposedPolicies;
+    }
 
-	public abstract boolean isEvaluable(ProbNet probNet);
+    /**
+     * @param imposedPolicies the imposedPolicies to set
+     */
+    public void setImposedPolicies (ArrayList<Potential> imposedPolicies)
+    {
+        this.imposedPolicies = imposedPolicies;
+    }
 
+    /**
+     * This method calculates the probabilities of each variable of interest in
+     * this form: P(a|evidence), P(b|evidence) ...
+     * @param variablesOfInterest <code>ArrayList</code> of
+     *            <code>Variable</code>s.
+     * @param evidence <code>EvidenceCase</code>.
+     * @return A <code>HashMap</code> with key = a variable and value = a
+     *         potential
+     * @throws CanNotDoEditException
+     * @throws ConstraintViolationException
+     * @throws DoEditException
+     * @throws NotEvaluableNetworkException
+     * @throws WrongCriterionException
+     * @throws ProbNodeNotFoundException
+     * @throws WrongGraphStructureException
+     */
+    public abstract HashMap<Variable, Potential> getIndividualProbabilities (ArrayList<Variable> variablesOfInterest)
+        throws NotEnoughMemoryException,
+        NormalizeNullVectorException,
+        DoEditException,
+        ConstraintViolationException,
+        CanNotDoEditException,
+        NotEvaluableNetworkException,
+        NonProjectablePotentialException,
+        WrongCriterionException,
+        WrongGraphStructureException,
+        ProbNodeNotFoundException;;
 
-	
+    /**
+     * This method calculates the probabilities for all the variables in this
+     * form: P(a|evidence), P(b|evidence) ...
+     * @param evidence <code>EvidenceCase</code>.
+     * @return A <code>HashMap</code> with key = a variable and value = a
+     *         potential
+     * @throws CanNotDoEditException
+     * @throws ConstraintViolationException
+     * @throws DoEditException
+     * @throws NotEvaluableNetworkException
+     * @throws NonProjectablePotentialException
+     * @throws WrongCriterionException
+     * @throws ProbNodeNotFoundException
+     * @throws WrongGraphStructureException
+     */
+    public HashMap<Variable, Potential> getIndividualProbabilities ()
+        throws NotEnoughMemoryException,
+        NormalizeNullVectorException,
+        DoEditException,
+        ConstraintViolationException,
+        CanNotDoEditException,
+        NotEvaluableNetworkException,
+        NonProjectablePotentialException,
+        WrongCriterionException,
+        WrongGraphStructureException,
+        ProbNodeNotFoundException
+    {
+        ArrayList<Variable> variablesOfInterest = probNet.getChanceAndDecisionVariables ();
+        return getIndividualProbabilities (variablesOfInterest);
+    };
 
-	/**
-	 * @return the evidence
-	 */
-	public EvidenceCase getEvidence() {
-		return evidence;
-	}
+    public abstract StrategyUtilities getUtilityTables ()
+        throws NotEnoughMemoryException,
+        WrongGraphStructureException,
+        ConstraintViolationException,
+        CanNotDoEditException,
+        DoEditException,
+        NonProjectablePotentialException,
+        WrongCriterionException,
+        ProbNodeNotFoundException;
 
-	/**
-	 * @param evidence the evidence to set
-	 */
-	public void setEvidence(EvidenceCase evidence) {
-		this.evidence = evidence;
-	}
-	
-	/**
-	 * @return the imposedPolicies
-	 */
-	public ArrayList<Potential> getImposedPolicies() {
-		return imposedPolicies;
-	}
+    public Hashtable<Variable, Double> getExpectedUtilities ()
+        throws NotEnoughMemoryException,
+        WrongGraphStructureException,
+        ConstraintViolationException,
+        CanNotDoEditException,
+        DoEditException,
+        NonProjectablePotentialException,
+        WrongCriterionException,
+        NotEvaluableNetworkException,
+        ProbNodeNotFoundException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	/**
-	 * @param imposedPolicies the imposedPolicies to set
-	 */
-	public void setImposedPolicies(ArrayList<Potential> imposedPolicies) {
-		this.imposedPolicies = imposedPolicies;
-	}
-	
-
-	/** This method calculates the probabilities of each variable of interest in
-	 *   this form: P(a|evidence), P(b|evidence) ...
-	 * @param variablesOfInterest <code>ArrayList</code> of 
-	 *   <code>Variable</code>s.
-	 * @param evidence <code>EvidenceCase</code>.
-	 * @return A <code>HashMap</code> with key = a variable and value = 
-	 *   a potential 
-	 * @throws CanNotDoEditException 
-	 * @throws ConstraintViolationException 
-	 * @throws DoEditException 
-	 * @throws NotEvaluableNetworkException 
-	 * @throws WrongCriterionException 
-	 * @throws ProbNodeNotFoundException 
-	 * @throws WrongGraphStructureException */
-	public abstract HashMap<Variable, Potential> getIndividualProbabilities(
-			ArrayList<Variable> variablesOfInterest) 
-			throws NotEnoughMemoryException, NormalizeNullVectorException,
-			DoEditException, ConstraintViolationException,
-			CanNotDoEditException, NotEvaluableNetworkException, 
-			NonProjectablePotentialException, WrongCriterionException, WrongGraphStructureException, ProbNodeNotFoundException;;
-	
-	/** This method calculates the probabilities for all the variables in
-	 *   this form: P(a|evidence), P(b|evidence) ...
-	 * @param evidence <code>EvidenceCase</code>.
-	 * @return A <code>HashMap</code> with key = a variable and value =
-	 *   a potential 
-	 * @throws CanNotDoEditException 
-	 * @throws ConstraintViolationException 
-	 * @throws DoEditException 
-	 * @throws NotEvaluableNetworkException 
-	 * @throws NonProjectablePotentialException 
-	 * @throws WrongCriterionException 
-	 * @throws ProbNodeNotFoundException 
-	 * @throws WrongGraphStructureException */
-	public HashMap<Variable, Potential> getIndividualProbabilities() 
-			throws NotEnoughMemoryException, NormalizeNullVectorException,
-			DoEditException, ConstraintViolationException, 
-			CanNotDoEditException, NotEvaluableNetworkException, 
-			NonProjectablePotentialException, WrongCriterionException, WrongGraphStructureException, ProbNodeNotFoundException
-			 {
-				ArrayList<Variable> variablesOfInterest =
-					probNet.getChanceAndDecisionVariables();
-				return getIndividualProbabilities(variablesOfInterest);
-			};
-			
-	
-	
-	public abstract StrategyUtilities getUtilityTables() throws NotEnoughMemoryException, WrongGraphStructureException, ConstraintViolationException, CanNotDoEditException, DoEditException, NonProjectablePotentialException, WrongCriterionException, ProbNodeNotFoundException;
-
-
-	public Hashtable<Variable, Double> getExpectedUtilities()
-			throws NotEnoughMemoryException, WrongGraphStructureException,
-			ConstraintViolationException, CanNotDoEditException,
-			DoEditException, NonProjectablePotentialException,
-			WrongCriterionException, NotEvaluableNetworkException,
-			ProbNodeNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public Double getGlobalExpectedUtility() throws NotEnoughMemoryException,
-			WrongGraphStructureException, ConstraintViolationException,
-			CanNotDoEditException, DoEditException,
-			NonProjectablePotentialException, WrongCriterionException, ProbNodeNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
+    public Double getGlobalExpectedUtility ()
+        throws NotEnoughMemoryException,
+        WrongGraphStructureException,
+        ConstraintViolationException,
+        CanNotDoEditException,
+        DoEditException,
+        NonProjectablePotentialException,
+        WrongCriterionException,
+        ProbNodeNotFoundException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- src/main/java/org/openmarkov/core/inference/annotation/InferenceManager.java
+++ src/main/java/org/openmarkov/core/inference/annotation/InferenceManager.java
@@ -1,72 +1,144 @@
+/*
+ * Copyright 2011 CISIAD, UNED, Spain Licensed under the European Union Public
+ * Licence, version 1.1 (EUPL) Unless required by applicable law, this code is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
+ */
+
 package org.openmarkov.core.inference.annotation;
 
+import java.lang.annotation.AnnotationFormatError;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-
-import org.openmarkov.core.io.format.annotation.FormatType;
+import org.openmarkov.core.inference.InferenceAlgorithm;
+import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.plugin.PluginLoader;
 import org.openmarkov.plugin.service.FilterIF;
 import org.openmarkov.plugin.service.PluginLoaderIF;
+
 /**
- * This class is the manager of the inference annotations. Detects the plugins with InferenceType 
- * annotations.
- * @see InferenceType
+ * This class is the manager of the inference annotations. Detects the plugins
+ * with InferenceAnnotation annotations.
+ * @see InferenceAnnotation
  * @author mpalacios
  * @author myebra
- *
+ * @author ibermejo
  */
+public class InferenceManager
+{
+    /**
+     * The plugin loader
+     */
+    private PluginLoaderIF                                       pluginsLoader;
+    /**
+     * The list of plugins detected in the project
+     */
+    private HashMap<String, Class<? extends InferenceAlgorithm>> inferenceAlgorithms;
 
-public class InferenceManager {
+    /**
+     * Constructor for InferenceManager.
+     */
+    @SuppressWarnings("unchecked")
+    public InferenceManager ()
+    {
+        super ();
+        this.pluginsLoader = new PluginLoader ();
+        this.inferenceAlgorithms = new HashMap<String, Class<? extends InferenceAlgorithm>> ();
+        for (Class<?> InferenceAlgorithmClass : findAllInferencePlugins ())
+        {
+            InferenceAnnotation lAnnotation = InferenceAlgorithmClass.getAnnotation (InferenceAnnotation.class);
+            if (InferenceAlgorithm.class.isAssignableFrom (InferenceAlgorithmClass))
+            {
+                inferenceAlgorithms.put (lAnnotation.name (),
+                                         (Class<? extends InferenceAlgorithm>) InferenceAlgorithmClass);
+            }
+            else
+            {
+                throw new AnnotationFormatError (
+                                                 "InferenceType annotation must be in a class that extends InferenceAlgorithm");
+            }
+        }
+    }
 
-	private static InferenceManager instance = null;
-	/**
-	 * The plugin loader
-	 */
-	private PluginLoaderIF pluginsLoader;
-	/**
-	 *  The list of plugins detected in the project
-	 */
-	private List<Class<?>> plugins;
+    /**
+     * Returns the list of the names of the algorithms that can evaluate the
+     * given instance of ProbNet
+     * @param probNet
+     * @return
+     * @throws SecurityException
+     * @throws NoSuchMethodException
+     */
+    public ArrayList<String> getInferenceAlgorithms (ProbNet probNet)
+        throws SecurityException,
+        NoSuchMethodException
+    {
+        ArrayList<String> inferenceAlgorithmNames = new ArrayList<String> ();
+        for (String algorithmName : inferenceAlgorithms.keySet ())
+        {
+            Constructor<? extends InferenceAlgorithm> constructor = inferenceAlgorithms.get (algorithmName).getConstructor (ProbNet.class);
+            if (constructor != null)
+            {
+                try
+                {
+                    constructor.newInstance (probNet);
+                    inferenceAlgorithmNames.add (algorithmName);
+                }
+                catch (Exception e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace ();
+                }
+            }
+        }
+        return inferenceAlgorithmNames;
+    }
 
-	/**
-	 * Gets a InferenceManager instance
-	 */
-	private InferenceManager ()
-	{
-		super ();
-		this.pluginsLoader = new PluginLoader ();
-		this.plugins = findAllInferencePlugins ();
-	}    
+    /**
+     * Returns an instance of the algorithm whose names we receive as a
+     * parameter, given the ProbNet
+     * @param algorithmName
+     * @param probNet
+     * @return
+     * @throws SecurityException
+     * @throws NoSuchMethodException
+     */
+    public InferenceAlgorithm getInferenceAlgorithmByName (String algorithmName, ProbNet probNet)
+        throws SecurityException,
+        NoSuchMethodException
+    {
+        InferenceAlgorithm instance = null;
+        Constructor<? extends InferenceAlgorithm> constructor = inferenceAlgorithms.get (algorithmName).getConstructor (ProbNet.class);
+        if (constructor != null)
+        {
+            try
+            {
+                instance = constructor.newInstance (probNet);
+            }
+            catch (Exception e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace ();
+            }
+        }
+        return instance;
+    }
 
-
-	public static InferenceManager getInstance()
-	{
-		if(instance == null)
-		{
-			instance = new InferenceManager ();
-		}
-		return instance;
-	}
-
-
-	/**
-	 * This method gets all the plugins with InferenceType annotations
-	 * @return a list with the plugins detected with InferenceType annotations.
-	 */
-	private final  List<Class<?>> findAllInferencePlugins ()
-	{
-		try
-		{
-			FilterIF filter = org.openmarkov.plugin.Filter.filter().toBeAnnotatedBy (
-					InferenceType.class);
-			return pluginsLoader.loadAllPlugins (filter);          
-		}
-		catch (Exception e) {}
-		return null;
-	}
-
-
-
-	
-
+    /**
+     * This method gets all the plugins with InferenceType annotations
+     * @return a list with the plugins detected with InferenceType annotations.
+     */
+    private final List<Class<?>> findAllInferencePlugins ()
+    {
+        try
+        {
+            FilterIF filter = org.openmarkov.plugin.Filter.filter ().toBeAnnotatedBy (InferenceAnnotation.class);
+            return pluginsLoader.loadAllPlugins (filter);
+        }
+        catch (Exception e)
+        {
+        }
+        return null;
+    }
 }

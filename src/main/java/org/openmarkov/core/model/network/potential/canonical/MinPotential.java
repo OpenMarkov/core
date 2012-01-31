509c3fd24ff09332bd666793f157ba18e8a90a5b
ibermejo <ibermejo@localhost>
2012-01-31 16:41:41 +0100

    Changed name of types Min and Max to GeneralizedMin and GeneralizedMax  in the annotation - Forgot to save before commiting!

--- src/main/java/org/openmarkov/core/model/network/potential/canonical/MinPotential.java
+++ src/main/java/org/openmarkov/core/model/network/potential/canonical/MinPotential.java
@@ -18,7 +18,7 @@ import org.openmarkov.core.model.network.potential.PotentialType;
 import org.openmarkov.core.model.network.potential.TablePotential;
 import org.openmarkov.core.model.network.potential.plugin.RelationType;
 
-@RelationType(name="Generalized Min", family="ICI")
+@RelationType(name="GeneralizedMin", family="ICI")
 public class MinPotential extends MinMaxPotential {
 
 	/** @param model. <code>ICIModel</code>.

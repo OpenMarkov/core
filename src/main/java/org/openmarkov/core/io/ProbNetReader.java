d44699edec7d1c9922581c472674142937f72424
ibermejo <ibermejo@localhost>
2012-03-23 17:58:15 +0100

    Added EvidenceCases to Parser & Writer

--- src/main/java/org/openmarkov/core/io/ProbNetReader.java
+++ src/main/java/org/openmarkov/core/io/ProbNetReader.java
@@ -10,12 +10,11 @@
 package org.openmarkov.core.io;
 
 import org.openmarkov.core.exception.ParserException;
-import org.openmarkov.core.model.network.ProbNet;
 
 public interface ProbNetReader {
 
 	/** @param netName = path + network name + extension. <code>String</code>
-	 * @return A <code>ProbNet</code> or <code>null</code> */ 
-	public ProbNet loadProbNet(String netName) throws ParserException;
+	 * @return A <code>ProbNetInfo</code> or <code>null</code> */ 
+	public ProbNetInfo loadProbNet(String netName) throws ParserException;
 	
 }
--- src/main/java/org/openmarkov/core/io/ProbNetWriter.java
+++ src/main/java/org/openmarkov/core/io/ProbNetWriter.java
@@ -9,14 +9,25 @@
 
 package org.openmarkov.core.io;
 
+import java.util.ArrayList;
+
 import org.openmarkov.core.exception.WriterException;
+import org.openmarkov.core.model.network.EvidenceCase;
 import org.openmarkov.core.model.network.ProbNet;
 
 public interface ProbNetWriter {
 
-	/** @param netName = path + network name + extension.
-	 * @param probNet. <code>ProbNet</code> <code>String</code> */
-	public void writeProbNet(String netName, ProbNet probNet) 
-			throws WriterException;
-		
+    /**
+     * @param netName = path + network name + extension.
+     * @param probNet. <code>ProbNet</code> <code>String</code>
+     */
+    public void writeProbNet (String netName, ProbNet probNet)
+        throws WriterException;
+
+    /**
+     * @param netName = path + network name + extension.
+     * @param probNet. <code>ProbNet</code> <code>String</code>
+     */
+    public void writeProbNet (String netName, ProbNet probNet, ArrayList<EvidenceCase> evidence)
+        throws WriterException;	
 }

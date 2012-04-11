9058a1a86e77b0717bcdc2a92c7936004dbecae6
ibermejo <ibermejo@localhost>
2012-04-11 19:14:54 +0200

    Uncomment pom part about repositories of our Nexus server

---
+++ src/main/java/org/openmarkov/core/model/graph/Link.java
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2011 CISIAD, UNED, Spain
+ *
+ * Licensed under the European Union Public Licence, version 1.1 (EUPL)
+ *
+ * Unless required by applicable law, this code is distributed
+ * on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
+ */
+
+package org.openmarkov.core.model.graph;
+
+import java.util.ArrayList;
+
+import org.openmarkov.core.exception.NotEnoughMemoryException;
+import org.openmarkov.core.model.network.PartitionedInterval;
+import org.openmarkov.core.model.network.ProbNode;
+import org.openmarkov.core.model.network.State;
+import org.openmarkov.core.model.network.Variable;
+import org.openmarkov.core.model.network.VariableType;
+import org.openmarkov.core.model.network.potential.Potential;
+import org.openmarkov.core.model.network.potential.PotentialRole;
+import org.openmarkov.core.model.network.potential.TablePotential;
+
+/**
+ * This class implements explicit links.
+ * 
+ * @author manuel
+ * @author fjdiez
+ * @version 1.0
+ * @since OpenMarkov 1.0
+ * @see openmarkov.graphs.Node
+ * @see openmarkov.graphs.Graph
+ */
+public class Link {
+
+	// Attributes
+	/** The first node. If the link is directed, this node is the parent. */
+	private Node node1;
+
+	/** The first node. If the link is directed, this node is the parent. */
+	private Node node2;
+
+	/** If true, the link is directed. Otherwise, it is an undirected link. */
+	private boolean directed;
+
+	/***
+	 * If true, the link has a link restriction.
+	 */
+	private boolean linkRestriction;
+
+	/****
+	 * Potential that contains the value of compatibility for the combinations
+	 * of the variables of node1 and node2
+	 */
+	private TablePotential restrictionsPotential;
+
+	/*****
+	 * List of revealing values of type state
+	 */
+	private ArrayList<State> revealingStates;
+
+	/*****
+	 * List of revealing values of type interval
+	 */
+	private ArrayList<PartitionedInterval> revealingIntervals;
+
+	// Constructors
+	/**
+	 * Creates an unlabelled link and sets the cross references in the nodes.
+	 * This constructor should be called only from the <code>addLink</code>
+	 * function in the class Graph.
+	 * 
+	 * @param node1
+	 *            <code>Node</code>.
+	 * @param node2
+	 *            <code>Node</code>.
+	 * @param directed
+	 *            <code>boolean</code>.
+	 * @argCondition Both nodes must belong to the same graph.
+	 */
+	public Link(Node node1, Node node2, boolean directed) {
+		Graph graph = node1.getGraph();
+		this.node1 = node1;
+		this.node2 = node2;
+		this.directed = directed;
+		graph.uf_addImplicitLink(this);
+		node1.uf_addLink(this);
+		node2.uf_addLink(this);
+		revealingStates = new ArrayList<State>();
+		revealingIntervals = new ArrayList<PartitionedInterval>();
+		linkRestriction = false;
+	}
+
+	// Methods
+	/**
+	 * @return The parent (if the link is directed) or the first node (if the
+	 *         link is undirected).
+	 * @consultation
+	 */
+	public Node getNode1() {
+		return node1;
+	}
+
+	/**
+	 * @return The child (if the link is directed) or the second node (if the
+	 *         link is undirected).
+	 * @consultation
+	 */
+	public Node getNode2() {
+		return node2;
+	}
+
+	/**
+	 * @param node
+	 *            <code>Node</code>.
+	 * @return <code>true</code> if the link contains <code>node</code>.
+	 * @consultation
+	 */
+	public boolean contains(Node node) {
+		return ((node1 == node) || (node2 == node));
+	}
+
+	/**
+	 * @return <code>true</code> if the link is directed, false if it is
+	 *         undirected
+	 * @consultation
+	 */
+	public boolean isDirected() {
+		return directed;
+	}
+
+	/******
+	 * @return<code>true</code> if the link has a linkRestriction
+	 *                          associates,false otherwise
+	 * @consultation
+	 */
+	public boolean hasRestrictions() {
+		return linkRestriction;
+	}
+
+	/**
+	 * Initializes a TablePotential for the variable associated to node1 and
+	 * node2, whose values are all 1.
+	 * 
+	 * @throws NotEnoughMemoryException
+	 */
+	public void initializesRestrictionsPotential()
+			throws NotEnoughMemoryException {
+		ArrayList<Variable> variables = new ArrayList<Variable>();
+		variables.add(((ProbNode) node1.getObject()).getVariable());
+		variables.add(((ProbNode) node2.getObject()).getVariable());
+		restrictionsPotential = new TablePotential(variables,
+				PotentialRole.LINK_RESTRICTION);
+		linkRestriction = true;
+	}
+
+	/*****
+	 * Resets the TablePotential for the variables associated to node1 and node2
+	 * to its initial state.
+	 * 
+	 * @throws NotEnoughMemoryException
+	 */
+	public void resetRestrictionsPotential() throws NotEnoughMemoryException {
+		ArrayList<Variable> variables = new ArrayList<Variable>();
+		variables.add(((ProbNode) node1.getObject()).getVariable());
+		variables.add(((ProbNode) node2.getObject()).getVariable());
+		restrictionsPotential = new TablePotential(variables,
+				PotentialRole.LINK_RESTRICTION);
+		linkRestriction = false;
+	}
+
+	/*****
+	 * Assigns the value of the parameter compatibility to the combination of
+	 * the variables state1 and state2.
+	 * 
+	 * @param state1
+	 *            state of the variable of node1
+	 * @param state2
+	 *            state of the variable of node2
+	 * @param compatibility
+	 *            value of compatibility
+	 */
+	public void setCompatibilityValue(State state1, State state2,
+			int compatibility) {
+		int[] indexes = new int[2];
+		indexes[0] = restrictionsPotential.getVariable(0).getStateIndex(state1);
+		indexes[1] = restrictionsPotential.getVariable(1).getStateIndex(state2);
+		ArrayList<Variable> variables = restrictionsPotential.getVariables();
+		restrictionsPotential.setValue(variables, indexes, compatibility);
+	}
+
+	/******
+	 * Returns the compatibility value of the combination of state1 and state2.
+	 * 
+	 * @param state1
+	 *            state of the variable of node1.
+	 * @param state2
+	 *            state of the variable of node2.
+	 * @return the value 1 for compatibility and 0 for incompatibility.
+	 */
+
+	public int areCompatible(State state1, State state2) {
+		int[] indexes = new int[2];
+		indexes[0] = restrictionsPotential.getVariable(0).getStateIndex(state1);
+		indexes[1] = restrictionsPotential.getVariable(1).getStateIndex(state2);
+		ArrayList<Variable> variables = restrictionsPotential.getVariables();
+
+		return (int) restrictionsPotential.getValue(variables, indexes);
+
+	}
+
+	/****
+	 * 
+	 * @return the potential of the the link restriction.
+	 */
+	public Potential getRestrictionsPotential() {
+		return restrictionsPotential;
+	}
+
+	/****
+	 * Assigns the potential to the restrictionPotential of the link
+	 * 
+	 * @param potential
+	 */
+
+	public void setRestrictionsPotential(Potential potential) {
+		this.restrictionsPotential = (TablePotential) potential;
+	}
+
+	/** @return String */
+	public String toString() {
+		StringBuffer buffer = new StringBuffer(node1.toString());
+		if (directed) {
+			buffer.append(" --- ");
+		} else {
+			buffer.append(" --> ");
+		}
+		buffer.append(node2.toString());
+		return buffer.toString();
+	}
+
+	/*****
+	 * This method indicates whether there are revealing conditions for the
+	 * link.
+	 * 
+	 * @return <code>true</code> if there exist revealing conditions.
+	 */
+	public boolean hasRevealingConditions() {
+
+		VariableType varType = ((ProbNode) node1.getObject()).getVariable()
+				.getVariableType();
+		if (varType.equals(VariableType.NUMERIC)) {
+			return !revealingIntervals.isEmpty();
+		} else {
+			return !revealingStates.isEmpty();
+		}
+	}
+
+	/**
+	 * @return the revealingStates
+	 */
+	public ArrayList<State> getRevealingStates() {
+		return revealingStates;
+	}
+
+	/**
+	 * @param revealingStates
+	 *            the revealingStates to set
+	 */
+	public void setRevealingStates(ArrayList<State> revealingStates) {
+		this.revealingStates = revealingStates;
+	}
+
+	/**
+	 * @return the revealingIntervals
+	 */
+	public ArrayList<PartitionedInterval> getRevealingIntervals() {
+		return revealingIntervals;
+	}
+
+	/**
+	 * @param revealingIntervals
+	 *            the revealingIntervals to set
+	 */
+	public void setRevealingIntervals(
+			ArrayList<PartitionedInterval> revealingIntervals) {
+		this.revealingIntervals = revealingIntervals;
+	}
+
+	/*****
+	 * Adds the state to the revealing condition list.
+	 * 
+	 * @param state
+	 */
+	public void addRevealingState(State state) {
+
+		revealingStates.add(state);
+	}
+
+	/*****
+	 * Removes the revealing state from the revealing condition list.
+	 * 
+	 * @param state
+	 */
+	public void removeRevealingState(State state) {
+		revealingStates.remove(state);
+
+	}
+
+	/*****
+	 * Adds the interval to the revealing condition list.
+	 * 
+	 * @param interval
+	 */
+	public void addRevealingInterval(PartitionedInterval interval) {
+		this.revealingIntervals.add(interval);
+	}
+
+	/********
+	 * Removes the interval from the revealing condition list.
+	 * 
+	 * @param interval
+	 */
+	public void removeRevealingInterval(PartitionedInterval interval) {
+		this.revealingIntervals.remove(interval);
+	}
+
+}
---
+++ src/main/java/org/openmarkov/core/model/network/ProbNetOperations.java
@@ -0,0 +1,303 @@
+/*
+* Copyright 2011 CISIAD, UNED, Spain
+*
+* Licensed under the European Union Public Licence, version 1.1 (EUPL)
+*
+* Unless required by applicable law, this code is distributed
+* on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
+*/
+
+package org.openmarkov.core.model.network;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Stack;
+
+import org.openmarkov.core.exception.NonProjectablePotentialException;
+import org.openmarkov.core.exception.NotEnoughMemoryException;
+import org.openmarkov.core.exception.WrongCriterionException;
+import org.openmarkov.core.model.graph.Node;
+import org.openmarkov.core.model.network.potential.Potential;
+
+/** This class performs prune on <code>ProbNet</code>  */
+public class ProbNetOperations {
+
+	// Methods
+	/** Performs prune operation in these steps:
+	 * <ol>
+	 * <li> Copy the received <code>ProbNet</code>.
+	 * <li> Remove barren nodes from the copied <code>ProbNet</code>.
+	 * <li> Remove unreachable nodes from <code>variablesOfInterest</code> given
+	 *  the <code>variablesOfEvidence</code>.
+	 * </ol>
+	 * @return <code>ProbNet</code>. Evidence variables are removed in serial 
+	 * connections */
+	public static ProbNet getPruned(ProbNet probNet, 
+			Collection<Variable> variablesOfInterest,
+			EvidenceCase evidence) {
+        ProbNet prunedProbNet = probNet.copy ();
+        HashSet<Variable> variablesOfInterest2 = new HashSet<Variable> (variablesOfInterest);
+        HashSet<Variable> variablesOfEvidence2 = new HashSet<Variable> (evidence.getVariables ());
+        prunedProbNet = removeBarrenNodes (prunedProbNet, variablesOfInterest2,
+                                           variablesOfEvidence2);
+        prunedProbNet = removeUnreachableNodes (prunedProbNet, variablesOfInterest2,
+                                                variablesOfEvidence2);
+        return prunedProbNet;
+	}
+	
+	/**Projects the evidence in the <code>probNet</code> potentials and remove
+	 * evidence variables
+	 * @param probNet. <code>ProbNet</code>
+	 * @param evidence. <code>EvidenceCase</code>
+	 * @throws NotEnoughMemoryException */
+    public static void projectEvidence (ProbNet probNet, EvidenceCase evidence)
+        throws NotEnoughMemoryException
+    {
+        ArrayList<Variable> variables = evidence.getVariables ();
+        for (Variable variable : variables)
+        {
+            ArrayList<Potential> potentials = probNet.getPotentials (variable);
+            for (Potential potential : potentials)
+            {
+                probNet.removePotential (potential);
+                try
+                {
+                    for (Potential newPotential : potential.tableProject (evidence, null))
+                    {
+                        if (newPotential.getNumVariables () > 0)
+                        {
+                            boolean containVariables = true;
+                            for (Variable potentialVariable : newPotential.getVariables ())
+                            {
+                                containVariables &= (probNet.getProbNode (potentialVariable) != null);
+                            }
+                            if (containVariables)
+                            {
+                                probNet.addPotential (newPotential);
+                            }
+                        }
+                    }
+                }
+                catch (NonProjectablePotentialException e)
+                {
+                    e.printStackTrace (); // Unreachable code
+                }
+                catch (WrongCriterionException e)
+                {
+                    e.printStackTrace (); // Unreachable code
+                }
+            }
+            probNet.removeProbNode (probNet.getProbNode (variable));
+        }
+    }
+
+	/** Remove nodes that:<ol>
+	 * <li> Are not included in <code>variablesOfInterest</code>
+	 * <li> Are not included in <code>variablesOfEvidence</code>
+	 * <li> Have no children or all its children are barren nodes.
+	 * </ol>
+	 * @param variablesOfEvidence2 
+	 * @param variablesOfInterest2 
+	 * @param prunedProbNet. <code>ProbNet</code>
+	 * @return <code>ProbNet</code> without barren nodes. */
+    private static ProbNet removeBarrenNodes (ProbNet prunedProbNet,
+                                              Collection<Variable> variablesOfInterest,
+                                              HashSet<Variable> variablesOfEvidence)
+    {
+        ArrayList<ProbNode> barrenNodes = new ArrayList<ProbNode> ();
+        boolean foundBarrenNodes = false;
+        // TODO Instead of looping through the whole network each time, examine
+        // the parents of each barren node removed to see if they have become
+        // barren nodes
+        do
+        {
+            // Collect barren nodes
+            ArrayList<ProbNode> probNodes = prunedProbNet.getProbNodes ();
+            for (ProbNode probNode : probNodes)
+            {
+                Node node = probNode.getNode ();
+                if (node.getNumChildren () == 0)
+                {
+                    Variable variable = probNode.getVariable ();
+                    if (!variablesOfInterest.contains (variable)
+                        && !variablesOfEvidence.contains (variable))
+                    {
+                        barrenNodes.add (probNode);
+                    }
+                }
+            }
+            foundBarrenNodes = barrenNodes.size () > 0;
+            if (foundBarrenNodes)
+            {
+                // Remove barren nodes
+                for (ProbNode probNode : barrenNodes)
+                {
+                    prunedProbNet.removeProbNode (probNode);
+                }
+                barrenNodes.clear ();
+            }
+        }
+        while (foundBarrenNodes);
+        return prunedProbNet;
+    }
+
+    /**
+     * Removes the nodes that are not connected to the variables of interest by
+     * any path
+     * @param probNet
+     * @param variablesOfInterest
+     * @param variablesOfEvidence
+     * @return
+     */
+	private static ProbNet removeUnreachableNodes(ProbNet probNet, 
+			Collection<Variable> variablesOfInterest, 
+			HashSet<Variable> variablesOfEvidence) {
+		// Gets nodes of interest and adds nodes connected to them
+		Stack<Node> nodesToExplore = new Stack<Node>();
+		HashSet<Node> nodesToKeep = new HashSet<Node>();
+
+		// Store nodes of variablesOfInterest in nodesToKeep
+		for (Variable variable : variablesOfInterest) {
+			Node node = probNet.getProbNode(variable).getNode();
+			nodesToKeep.add(node);
+		}		
+
+		// Add neighbors of variablesOfInterest as nodesToKeep and store them in
+		// nodesToExplore
+		@SuppressWarnings("unchecked")
+		HashSet<Node> nodesToKeepClon = (HashSet<Node>)nodesToKeep.clone();
+		for (Node node : nodesToKeepClon) {
+			ArrayList<Node> neighbors = node.getNeighbors();
+			for (Node neighbor : neighbors) {
+				if (!nodesToKeep.contains(neighbor)) {
+					nodesToKeep.add(neighbor);
+					nodesToExplore.push(neighbor);
+				}
+			}
+		}
+		
+		// Store evidence nodes and probNodes in collections
+		HashSet<Node> hashEvidenceNodes = 
+				getEvidenceNodes(probNet, variablesOfEvidence);
+		HashSet<Node> evidenceAndAncestors = getAncestors(hashEvidenceNodes);
+		
+		// For each interest node, finds connected nodes via valid paths.
+		while (!nodesToExplore.empty()) {
+			Node node = nodesToExplore.pop();
+			
+			// Find head to head connected nodes: X->Y<-Z and
+			// Y is evidence or Y has a descendent that is evidence
+			if (evidenceAndAncestors.contains(node)) {
+				ArrayList<Node> parents = node.getParents();
+				int parentsSize = parents.size();
+				boolean interestI, interestJ;
+				for (int i = 0; i < parentsSize - 1; i++) {
+					Node parentI = parents.get(i);
+					interestI = nodesToKeep.contains(parentI);
+					for (int j = 1; j < parentsSize; j++) {
+						Node parentJ = parents.get(j);
+						interestJ = nodesToKeep.contains(parentJ);
+						if (interestI && !interestJ) {
+							nodesToExplore.push(parentJ);
+							nodesToKeep.add(parentJ);
+						} else if (!interestI && interestJ) {
+							nodesToExplore.push(parentI);
+							nodesToKeep.add(parentI);
+							interestI = true;
+						}
+					}
+				}
+			}
+			
+			// Find not head to head connected nodes:
+			// X->Y->Z, X<-Y<-Z and X<-Y->Z
+			if (!hashEvidenceNodes.contains(node)) {
+				ArrayList<Node> children = node.getChildren();
+				ArrayList<Node> parents = node.getParents();
+				int numChildren = children.size();
+				for (int i = 0; i < numChildren; i++) {
+					Node child = children.get(i);
+					boolean interestChild = nodesToKeep.contains(child);
+					// X->Y->Z and X<-Y<-Z
+					for (Node parent : parents) {
+						boolean interestParent = nodesToKeep.contains(parent);
+						if (interestChild && !interestParent) {
+							nodesToExplore.push(parent);
+							nodesToKeep.add(parent);
+						} else if (interestParent && !interestChild) {
+							nodesToExplore.push(child);
+							nodesToKeep.add(child);
+							interestChild = true;
+						}
+					}
+					// X<-Y->Z
+					for (int j = i + 1; j < numChildren; j++) {
+						Node child2 = children.get(j);
+						boolean interestChild2 = nodesToKeep.contains(child2);
+						if (interestChild2 && !interestChild) {
+							nodesToExplore.push(child);
+							nodesToKeep.add(child);
+							interestChild = true;
+						} else if (interestChild && !interestChild2) {
+							nodesToExplore.push(child2);
+							nodesToKeep.add(child2);
+						}
+					}
+				}
+			}
+		}
+
+		// remove nodes that are not in nodesToKeep in prunedProbNet
+		ArrayList<Node> prunedProbNetNodes = ProbNet
+				.getNodesOfProbNodes(probNet.getProbNodes());
+		for (Node node : prunedProbNetNodes) {
+			if (!nodesToKeep.contains(node)) {
+				probNet.removeProbNode((ProbNode) node.getObject());
+			}
+		}
+		
+		return probNet;
+	}
+	
+	
+	private static HashSet<Node> getEvidenceNodes(ProbNet prunedProbNet, 
+			Collection<Variable> variablesOfEvidence) {
+		HashSet<Node> hashEvidenceNodes = new HashSet<Node>();
+		for (Variable variable : variablesOfEvidence) {
+			ProbNode evidenceProbNode = prunedProbNet.getProbNode(variable);
+			if (evidenceProbNode != null) {
+				Node evidenceNode = evidenceProbNode.getNode();
+				hashEvidenceNodes.add(evidenceNode);
+			}
+		}
+		return hashEvidenceNodes;
+	}
+
+	/**
+	 * @param nodes
+	 *            . <code>ArrayList</code> of <code>Node</code>.
+	 * @return <code>nodes</code> and its ancestors. <code>ArrayList</code> of
+	 *         <code>Node</code>.
+	 */
+	private static HashSet<Node> getAncestors(Collection<Node> nodes) {
+		HashSet<Node> ancestors = new HashSet<Node>(nodes);
+
+		Stack<Node> noExploredNodes = new Stack<Node>();
+		noExploredNodes.addAll(nodes);
+
+		while (!noExploredNodes.empty()) {
+			Node node = noExploredNodes.pop();
+			ArrayList<Node> parents = node.getParents();
+			for (Node parent : parents) {
+				if (ancestors.add(parent)) {
+					noExploredNodes.push(parent);
+				}
+			}
+		}
+		return ancestors;
+	}
+
+
+	
+}

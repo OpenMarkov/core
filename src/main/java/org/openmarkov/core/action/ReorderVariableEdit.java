e25b9c93c0f1a06f0dd444efae6dacea431ebb42
maryebra <maryebra@localhost>
2012-06-20 13:52:35 +0200

    Added new edit to reorder variables, useful for the GUI

--- src/main/java/org/openmarkov/core/action/ReorderVariableEdit.java
+++ src/main/java/org/openmarkov/core/action/ReorderVariableEdit.java
@@ -9,25 +9,28 @@ import org.openmarkov.core.model.network.StringWithProperties;
 import org.openmarkov.core.model.network.Variable;
 import org.openmarkov.core.model.network.potential.PotentialRole;
 
+@SuppressWarnings("serial")
 public class ReorderVariableEdit  extends SimplePNEdit {
 
 	private Object [][]dataTable;
 	private ArrayList<Variable> oldVariables;
-	private ArrayList<Variable> newVariables;
+	private ArrayList<Variable> newVariables = new ArrayList<Variable>();
 	private StateAction stateAction;
 	private ProbNode probNode;
+	private ArrayList<Variable> variables = new ArrayList<Variable>();
 	
 	public ReorderVariableEdit(ProbNode probNode, Object data[][], StateAction stateAction) {
 		super(probNode.getProbNet());
 		this.probNode = probNode;
 		this.dataTable = data;
 		this.stateAction = stateAction;
+		this.oldVariables = (ArrayList<Variable>) probNode.getPotentials().get(0).getVariables().clone();
 		if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.CONDITIONAL_PROBABILITY) {
-			ArrayList<Variable> variables = (ArrayList<Variable>) probNode.getPotentials().get(0).getVariables().clone();
+			variables = (ArrayList<Variable>) probNode.getPotentials().get(0).getVariables().clone();
 			variables.remove(0);
-			this.oldVariables = variables;
+			
 		} else if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.UTILITY) {
-			this.oldVariables = (ArrayList<Variable>) probNode.getPotentials().get(0).getVariables().clone();
+			variables = (ArrayList<Variable>) probNode.getPotentials().get(0).getVariables().clone();
 		}
 		
 	}
@@ -38,24 +41,36 @@ public class ReorderVariableEdit  extends SimplePNEdit {
 		case DOWN:
 			ArrayList<Variable> newVariablesDown = new ArrayList<Variable>();
 			for (int i = 0; i < dataTable.length; i++) {
-				for (int j = 0; i < oldVariables.size(); j++) {
-					if ((String)dataTable[i][0] == oldVariables.get(j).getName()) {
-						newVariablesDown.add(oldVariables.get(j));
+				for (int j = 0; j < variables.size(); j++) {
+					if ((String)dataTable[i][0] == variables.get(j).getName()) {
+						newVariablesDown.add(variables.get(j));
 					}
 				}
 			}
-			probNode.getPotentials().get(0).setVariables(newVariablesDown);
+			if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.CONDITIONAL_PROBABILITY) {
+				this.newVariables.add(probNode.getPotentials().get(0).getVariables().get(0));
+				this.newVariables.addAll(newVariablesDown);
+			} else if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.UTILITY) {
+				this.newVariables.addAll(newVariablesDown);
+			}
+			probNode.getPotentials().get(0).setVariables(newVariables);
 			break;
 		case UP:
 			ArrayList<Variable> newVariablesUp = new ArrayList<Variable>();
 			for (int i = 0; i < dataTable.length; i++) {
-				for (int j = 0; i < oldVariables.size(); j++) {
-					if ((String)dataTable[i][0] == oldVariables.get(j).getName()) {
-						newVariablesUp.add(oldVariables.get(j));
+				for (int j = 0; j < variables.size(); j++) {
+					if ((String)dataTable[i][0] == variables.get(j).getName()) {
+						newVariablesUp.add(variables.get(j));
 					}
 				}
 			}
-			probNode.getPotentials().get(0).setVariables(newVariablesUp);
+			if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.CONDITIONAL_PROBABILITY) {
+				this.newVariables.add(probNode.getPotentials().get(0).getVariables().get(0));
+				this.newVariables.addAll(newVariablesUp);
+			} else if (probNode.getPotentials().get(0).getPotentialRole() == PotentialRole.UTILITY) {
+				this.newVariables.addAll(newVariablesUp);
+			}
+			probNode.getPotentials().get(0).setVariables(newVariables);
 			break;
 
 		}

dfeabdcfbd6967bea4ca5d40711abd31e0c8a72b
ibermejo <ibermejo@localhost>
2012-10-11 16:10:42 +0200

    New getVariable method in CaseDatabase

--- src/main/java/org/openmarkov/core/io/database/CaseDatabase.java
+++ src/main/java/org/openmarkov/core/io/database/CaseDatabase.java
@@ -12,7 +12,6 @@ package org.openmarkov.core.io.database;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.core.model.network.Variable;
 
 public class CaseDatabase
@@ -32,6 +31,22 @@ public class CaseDatabase
         this.cases = cases;
     }
         
+    public CaseDatabase (CaseDatabase database)
+    {
+        super();
+        this.variables = new ArrayList<> (database.getVariables ());
+        int[][] casesToCopy = database.getCases ();
+        this.cases = new int[casesToCopy.length][variables.size ()];
+        
+        for(int i = 0; i< cases.length; ++i)
+        {
+            for(int j = 0; j < cases[i].length; ++i)
+            {
+                cases[i][j] = casesToCopy[i][j];
+            }
+        }
+    }
+
     /**
      * Returns the cases.
      * @return the cases.
@@ -48,6 +63,26 @@ public class CaseDatabase
     public List<Variable> getVariables ()
     {
         return variables;
+    }
+    
+    /**
+     * Returns the variable given the name
+     * @param name
+     * @return
+     */
+    public Variable getVariable (String name)
+    {
+        Variable variable = null;
+        int i = 0;
+        while(variable == null && i < variables.size ())
+        {
+            if(variables.get (i).getName ().equals (name))
+            {
+                variable = variables.get(i);
+            }
+            ++i;
+        }
+        return variable;
     }
 
 }

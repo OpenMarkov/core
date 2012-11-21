72d7cacee9b64d08b95430e49fd6037cb04f482d
marias <marias@localhost>
2012-11-21 18:51:54 +0100

    Add javadoc to UniqueStack

--- src/main/java/org/openmarkov/core/model/network/UniqueStack.java
+++ src/main/java/org/openmarkov/core/model/network/UniqueStack.java
@@ -3,11 +3,14 @@ package org.openmarkov.core.model.network;
 import java.util.HashSet;
 import java.util.Stack;
 
-// This class stack ensure that each element is stored only once.
+/** This class stack ensures that each element is stored only once.
+ * @author marias */ 
 public class UniqueStack<T> {
 	private final Stack<T> stack = new Stack<T>();
 	private final HashSet<T> set = new HashSet<T>();
 
+	/** @param t template parameter. <code>T</code>
+	 * @return true (as specified by Collection.add) <code>boolean</code> */
 	public boolean push(T t) {
 		// Only add element to stack if the set does not contain the specified element.
 		if (set.add(t)) {
@@ -16,12 +19,14 @@ public class UniqueStack<T> {
 		return true;
 	}
 
+	/** @return t template parameter. <code>T</code> */
 	public T pop() {
 		T ret = stack.pop();
 		set.remove(ret);
 		return ret;
 	}
 	
+	/** @return <code>boolean</code> */
 	public boolean empty() {
 		return set.isEmpty();
 	}

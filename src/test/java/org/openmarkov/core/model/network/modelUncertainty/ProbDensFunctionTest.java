59b6ad97f51de051a86e1fd079e25dbd4fbc1dfd
mluque <mluque@localhost>
2013-03-04 18:48:23 +0100

    Created abstract test class for probability distributions.

---
+++ src/test/java/org/openmarkov/core/model/network/modelUncertainty/ProbDensFunctionTest.java
@@ -0,0 +1,26 @@
+/*
+* Copyright 2011 CISIAD, UNED, Spain
+*
+* Licensed under the European Union Public Licence, version 1.1 (EUPL)
+*
+* Unless required by applicable law, this code is distributed
+* on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
+*/
+package org.openmarkov.core.model.network.modelUncertainty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author manolo
+ *
+ */
+public class ProbDensFunctionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

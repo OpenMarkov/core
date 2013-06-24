de94ddcff16c4ad50a29460217db38f07405bea3
ibermejo <ibermejo@localhost>
2013-06-24 19:27:58 +0100

    Minor changes in Threshold

--- src/main/java/org/openmarkov/core/model/network/potential/treeadd/Threshold.java
+++ src/main/java/org/openmarkov/core/model/network/potential/treeadd/Threshold.java
@@ -1,16 +1,19 @@
 package org.openmarkov.core.model.network.potential.treeadd;
+
 /**
- * A threshold is defined by a float value and a boolean that indicates if the value delimits a closed or an open interval
- * on the left and on the right side. There are two possibilities )[, ](	
+ * A threshold is defined by a float value and a boolean that indicates if the
+ * value delimits a closed or an open interval on the left and on the right
+ * side. There are two possibilities )[, ](
  * 
  * It is used by TreeADDBranch when its topVariable is a numeric variable
+ * 
  * @author myebra
- *
+ * 
  */
 public class Threshold {
 	
 	private double limit;
-	private boolean belongsToLeft; // if false --> )[ ; if true --> ](
+	private boolean belongsToLeft; // if true --> ](; if false --> )[; 
 	
 	
 	public Threshold(double limit, boolean belongsToLeft){
@@ -33,31 +36,19 @@ public class Threshold {
 	/**@return true if the value is above the limit value of the threshold object
 	 * @param value to check
 	 * **/
-	public boolean isBelow(float value){
-		if (value > this.limit) {
-			return true;
-		} else { 
-			return false; 
-		}
+	public boolean isBelow(double value){
+	    return value > this.limit || (value == this.limit && !belongsToLeft);
 	}
 	
 	/**@return true if the value is below the limit value of the threshold object
 	 * @param value to check
 	 * **/
-	public boolean isAbove(float value){
-		if (value < this.limit) {
-			return true;
-		} else { 
-			return false;
-		}
+	public boolean isAbove(double value){
+	    return value < this.limit || (value == this.limit && belongsToLeft);
 	}
 	
 	public boolean equals (Threshold threshold) {
-		if (this.limit == threshold.getLimit() && this.belongsToLeft == threshold.belongsToLeft()) {
-			 return true;
-		} else {
-			return false;
-		}
+	    return limit == threshold.getLimit() && belongsToLeft == threshold.belongsToLeft();
 	}
 	
 	public Threshold copy() {

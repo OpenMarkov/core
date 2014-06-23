b5734219cb018e0ef2e44eda4e437165d68d057a
marias <marias@localhost>
2014-06-23 14:48:05 +0200

    Method createPerfectKnowledge in IDFactory

--- src/test/java/org/openmarkov/core/model/network/potential/operation/IDFactory.java
+++ src/test/java/org/openmarkov/core/model/network/potential/operation/IDFactory.java
@@ -12,7 +12,7 @@ import org.openmarkov.core.model.network.type.InfluenceDiagramType;
 
 /**
  * @author manuel
- *
+ * This class creates some influence diagrams for test purposes.
  */
 public class IDFactory {
 
@@ -27,25 +27,31 @@ public class IDFactory {
 		double[] diseaseValues = {0.2, 0.8};
 		TablePotential diseasePotential = new TablePotential(
 				diseaseVariables, PotentialRole.CONDITIONAL_PROBABILITY, diseaseValues) ;
+		perfectKnowledge.addPotential(diseasePotential);
 
 		// Decision variable
 		Variable therapy = new Variable("Therapy", "no", "yes");
+		perfectKnowledge.addNode(therapy, NodeType.DECISION);
 		
 		// Create health state variable and potential
 		Variable healthState = new Variable("Health state");
+		perfectKnowledge.addNode(healthState, NodeType.UTILITY);
 		List<Variable> healthStateVariables = new ArrayList<Variable>(2);
 		healthStateVariables.add(therapy);
 		healthStateVariables.add(disease);
 		TablePotential healthStatePotential = new TablePotential(healthState, healthStateVariables);
+		healthStatePotential.values = new double[]{10.0, 9.0, 3.0, 8.0};
+		perfectKnowledge.addPotential(healthStatePotential);
 
 		// Create cost of therapy variable and potential
 		Variable costOfTherapy = new Variable("Cost of therapy");
+		perfectKnowledge.addNode(costOfTherapy, NodeType.UTILITY);
 		List<Variable> costOfTherapyVariables = new ArrayList<Variable>(1);
 		costOfTherapyVariables.add(therapy);
 		TablePotential costOfTherapyPotential = new TablePotential(costOfTherapy, costOfTherapyVariables);
+		costOfTherapyPotential.values = new double[]{0.0, -0.25};
+		perfectKnowledge.addPotential(costOfTherapyPotential);
 
-		// Create probNet perfectKnowledge
-		// TODO
 		return perfectKnowledge;
 	}
 	

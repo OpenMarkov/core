617ace0361e3dda1d7a0033448296b87a0b0c999
jorgepmartin <jorgepmartin@gmail.com>
2016-01-19 09:37:47 +0100

    Refactoring sensitivity analysis to tasks. Fixed method ProbNet.addPotential

---
+++ src/main/java/org/openmarkov/core/inference/tasks/SensAnTornadoSpider.java
@@ -0,0 +1,70 @@
+package org.openmarkov.core.inference.tasks;
+
+import org.openmarkov.core.exception.IncompatibleEvidenceException;
+import org.openmarkov.core.exception.NotEvaluableNetworkException;
+import org.openmarkov.core.exception.UnexpectedInferenceException;
+import org.openmarkov.core.model.network.ProbNet;
+import org.openmarkov.core.model.network.Variable;
+import org.openmarkov.core.model.network.potential.Intervention;
+import org.openmarkov.core.model.network.potential.Potential;
+import org.openmarkov.core.model.network.potential.TablePotential;
+
+import java.util.HashMap;
+
+/**
+ * Created by Jorge on 28/09/2015.
+ */
+public class SensAnTornadoSpider extends Task {
+    /**
+     * @param probNet The network used in the inference
+     * @throws NotEvaluableNetworkException
+     */
+    public SensAnTornadoSpider(ProbNet probNet) throws NotEvaluableNetworkException {
+        super(probNet);
+    }
+
+    @Override
+    public Intervention getOptimalStrategy() throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public TablePotential getUtility() throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public TablePotential getGlobalUtility() throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public TablePotential getProbability() throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public HashMap<Variable, TablePotential> getPosteriorValues() throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public Potential getOptimizedPolicy(Variable decisionVariable) throws IncompatibleEvidenceException, UnexpectedInferenceException {
+        return null;
+    }
+
+    @Override
+    public boolean checkNetworkConsistency() throws NotEvaluableNetworkException {
+        return false;
+    }
+
+    @Override
+    public boolean checkEvidenceConsistency() {
+        return false;
+    }
+
+    @Override
+    public boolean checkPoliciesConsistency() {
+        return false;
+    }
+}

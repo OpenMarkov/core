61ab4da83eba0c724a22d2c30cce2d570afe5df9
carmenyago <carmenyago@localhost>
2016-11-03 19:11:36 +0100

    org.openmarkov.core.io.format.annotation.FormatType refacted to org.openmarkov.core.io.format.annotation.FormatTypeProbModelXML

--- src/main/java/org/openmarkov/core/io/format/annotation/FormatTypeProbModelXML.java
+++ src/main/java/org/openmarkov/core/io/format/annotation/FormatTypeProbModelXML.java
@@ -12,6 +12,7 @@ package org.openmarkov.core.io.format.annotation;
 /**
  * This class sets the labels for the annotations format
  * @author mpalacios
+ * @author carmenyago -- added the annotation type element <code>version</code>
  */
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -20,12 +21,23 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target (ElementType.TYPE)
-public @interface FormatType {
+public @interface FormatTypeProbModelXML {
     /**
      * Gets the name of the class
      * @return The name of the format
      */
     String name ();
+    
+    //CMI
+    /**
+     * Gets the version of the class. Sometimes as in the case of the PGMXReader/Writer 
+     * there are several classes representing several versions of something as, for example, 
+     * the ProbModelXML 
+     * @return The version of the format
+     */
+    String version ();
+    //CMF
+    
     /**
      * Gets the file extension that write/read the class
      * @return The format extension

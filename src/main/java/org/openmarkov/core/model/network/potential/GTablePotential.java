bd8fbc329d3b421ea77da4bb698b602a23daf9da
marias <marias@localhost>
2017-09-26 20:01:34 +0200

    Small refactoring

--- src/main/java/org/openmarkov/core/model/network/potential/GTablePotential.java
+++ src/main/java/org/openmarkov/core/model/network/potential/GTablePotential.java
@@ -72,51 +72,6 @@ public class GTablePotential<E> extends TablePotential {
         this (potential.getVariables (), potential.getPotentialRole ());
     }
     
-    
-    
-//	/** Overrides <code>toString</code> method. Mainly for test purposes */
-//	public String toString() {
-//		// writes the variables names
-//		String string = new String(numVariables + " Variables: ");
-//		if (numVariables > 0) {
-//			string = string + variables.get(0).getName();
-//			for (int i = 1; i < numVariables; i++) {
-//				string = string + ", " + variables.get(i).getName();
-//			}
-//			if (role == PotentialRole.UTILITY ) {
-//				string += " - Utility potential";
-//			}
-//			string = string + "\n";
-//
-//			// writes each configuration and its value
-//			int[] configuration = null;
-//			int configurationsPerLine = 1;
-//			for (int i = 0; dimensions != null && i < dimensions.length
-//					&& i < 2; i++) {
-//				configurationsPerLine *= dimensions[i];
-//			}
-//			int numElementsTable = elementTable.size();
-//			for (int i = 0; i < numElementsTable; i++) {
-//				if (dimensions != null) {
-//					configuration = getConfiguration(i);
-//				}
-//				string = string + "[";
-//				for (int j = 0; configuration != null
-//						&& j < configuration.length; j++) {
-//					string = string + configuration[j];
-//				}
-//				string = string + "]: " + elementTable.get(i).toString();
-//				if (((i + 1) % configurationsPerLine == 0)
-//						|| (i == numElementsTable - 1)) {
-//					string = string + "\n";
-//				} else {
-//					string = string + ", ";
-//				}
-//			}
-//		}
-//		return string;
-//	}
-
 	/** Overrides <code>toString</code> method. Mainly for test purposes */
 	public String toString() {
 		StringBuilder buffer = new StringBuilder();

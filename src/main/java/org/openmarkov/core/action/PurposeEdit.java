8ab18dfe39dd15c35a47c7382d86583434c677be
Jorge Pérez <jorgepmartin@gmail.com>
2018-02-26 10:56:19 +0100

    Modifying and including new license of OpenMarkov

--- src/main/java/org/openmarkov/core/action/PurposeEdit.java
+++ src/main/java/org/openmarkov/core/action/PurposeEdit.java
@@ -1,17 +1,16 @@
 /*
-* Copyright 2011 CISIAD, UNED, Spain
-*
-* Licensed under the European Union Public Licence, version 1.1 (EUPL)
-*
-* Unless required by applicable law, this code is distributed
-* on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
-*/
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
 
 package org.openmarkov.core.action;
 
 import org.openmarkov.core.exception.DoEditException;
 import org.openmarkov.core.model.network.Node;
-@SuppressWarnings("serial")
+
+@SuppressWarnings("serial")
 /**
  * <code>PurposeEdit</code> is a simple edit that allows modify
  * the node purpose property. 
--- src/main/java/org/openmarkov/core/model/network/modelUncertainty/BetaAlphaNFunction.java
+++ src/main/java/org/openmarkov/core/model/network/modelUncertainty/BetaAlphaNFunction.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
 package org.openmarkov.core.model.network.modelUncertainty;
 
 @ProbDensFunctionType(name = "BetaAlphaNFunction", univariateName = "Beta", isValidForProbabilities =false, isValidForNumeric=false, parameters = { "alpha", "N" })

5b171787d148c8595eb0a84a4da3e124eafa1b61
marias <marias@dia.uned.es>
2018-07-11 10:30:05 +0200

    Small changes

---
+++ src/main/java/org/openmarkov/core/model/network/Properties.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
+package org.openmarkov.core.model.network;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * This class is a wrapper of a HashMap(key=String,value=String).<p>
+ * It gives the possibility to store String attributes accessed by a name (the key).
+ */
+public class Properties {
+
+	// Attributes
+	protected HashMap<String, String> information;
+
+	// Constructor
+	public Properties() {
+		information = new HashMap<>();
+	}
+
+	// Methods
+
+	/**
+	 * @return The object stored with <code>key</code> or <code>null</code> if
+	 * it does not exists.
+	 */
+	public Object get(String key) {
+		return information.get(key);
+	}
+
+	/**
+	 * @param key   <code>String</code>
+	 * @param value <code>Object</code>
+	 */
+	public void put(String key, String value) {
+		information.put(key, value);
+	}
+
+	/**
+	 * @return The object stored with <code>key</code> or <code>null</code> if
+	 * it does not exists.
+	 */
+	public Object remove(String key) {
+		return information.remove(key);
+	}
+
+	public int size() {
+		return information.size();
+	}
+
+	/**
+	 * @return The set of keys. <code>Set</code> of <code>String</code>
+	 */
+	public Set<String> getKeySet() {
+		return information.keySet();
+	}
+
+}

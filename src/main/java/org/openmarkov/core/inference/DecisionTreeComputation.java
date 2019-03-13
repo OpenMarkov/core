acb096b84d370e1ed37a50019f69deb0cbeffc12
Manuel Luque <mluque@dia.uned.es>
2019-03-13 19:47:57 +0100

    Continue refactoring of DANs inference.

---
+++ src/main/java/org/openmarkov/core/inference/DecisionTreeComputation.java
@@ -0,0 +1,9 @@
+package org.openmarkov.core.inference;
+
+import org.openmarkov.core.dt.DecisionTreeNode;
+
+public interface DecisionTreeComputation {
+	
+	public DecisionTreeNode getDecisionTree();
+
+}

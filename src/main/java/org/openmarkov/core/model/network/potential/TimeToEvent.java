722f3fa9680be8904a08d95bbb4f573876da8fad
carmenyago <carmen.yago@gmail.com>
2019-09-15 19:55:01 +0200

    Renamed

--- src/main/java/org/openmarkov/core/model/network/potential/TimeToEvent.java
+++ src/main/java/org/openmarkov/core/model/network/potential/TimeToEvent.java
@@ -1,12 +1,16 @@
 package org.openmarkov.core.model.network.potential;
 
+import org.openmarkov.core.model.network.EvidenceCase;
+import org.openmarkov.core.model.network.Configuration;
 import org.openmarkov.core.model.network.State;
 import org.openmarkov.core.model.network.Variable;
+
+import java.util.ArrayList;
 
 /**
  * Interface implemented by Potentials used in DES models to represent and  calculate Time To Event behaviour
  */
-public interface TimeToEventPotentialInterface {
+public interface TimeToEvent {
     /**
      * Generate a TTE since the event is "triggered" by initial event, another event or change in state
      * The event will happend at triggering_time + TTE
@@ -16,6 +20,10 @@ public interface TimeToEventPotentialInterface {
      * @return
      */
     public double getTimeToEvent(Variable stateVariable, State stateValue, String event);
+
+    public double getTimeToEvent(Configuration configuration);
+
+    public double getTimeToEvent(EvidenceCase ev);
 
 
 }

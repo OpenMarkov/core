e3207c1cd21020a99f0bddb6b302d1c46cd5d477
carmenyago <carmen.yago@gmail.com>
2019-09-15 20:07:05 +0200

    Removed inheritance from TransitionTablePotential and added a field TableWithEventsPotential.

--- src/main/java/org/openmarkov/core/model/network/potential/TimeToEventTablePotential.java
+++ src/main/java/org/openmarkov/core/model/network/potential/TimeToEventTablePotential.java
@@ -1,10 +1,11 @@
 package org.openmarkov.core.model.network.potential;
 
-import org.openmarkov.core.exception.InvalidStateException;
 import org.openmarkov.core.exception.NonProjectablePotentialException;
 import org.openmarkov.core.exception.WrongCriterionException;
 import org.openmarkov.core.inference.InferenceOptions;
 import org.openmarkov.core.model.network.*;
+import org.openmarkov.core.model.network.modelUncertainty.ProbDensFunction;
+import org.openmarkov.core.model.network.modelUncertainty.ProbDensFunctionManager;
 import org.openmarkov.core.model.network.potential.plugin.PotentialType;
 
 import java.util.ArrayList;
@@ -16,34 +17,53 @@ import java.util.List;
  *  Transition class to be merged with the new structure of tables
  * There have to be at least one Event variable
  * @version 1.0 -24/03/2019- -cyago -
+ * @version 1.1 -13/09/2019  -cyago- inheritance from TransitionTablePotential changed for assotiation
  * @since OpenMarkov 3.0
 */
 
 
 @PotentialType(family ="Event", name = "TimeToEventTable")
-public class TimeToEventTablePotential extends TransitionTablePotential implements TimeToEventPotentialInterface {
+public class TimeToEventTablePotential extends Potential implements ImpossibleConfiguration, TimeToEvent {
 
-    protected TablePotential tablePotential;
+    private ProbDensFunction distribution;
+    private String distributionName;
+    private Variable distributionVariable;
+    private TransitionTablePotential tableWithEvents;
 
-    protected Variable eventAsStates;
+    public TimeToEventTablePotential(List<Variable> variables, PotentialRole role){
+        this(variables,role, "Exact");
 
-    /**
-     * Configurations of parents when this event cannot occur
-     */
-    List<EvidenceCase> impossibleConfigurations;
-
-
-    public TimeToEventTablePotential(List<Variable> variables, PotentialRole role) {
-        super(variables, role);
-        impossibleConfigurations = new ArrayList<EvidenceCase>();
     }
 
+
+    public TimeToEventTablePotential(List<Variable> variables, PotentialRole role, String distributionName){
+        super(variables,role);
+        this.setDistributionName(distributionName);
+        try {
+          distribution =  ProbDensFunctionManager.getUniqueInstance().getProbDensFunctionClass(distributionName).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        String[] s=  ProbDensFunctionManager.getUniqueInstance().getParameters(distributionName);
+        State[] states = new State[s.length];
+        for (int i=0; i< s.length;i++){
+            states[i] = new State(s[i]);
+        }
+        distributionVariable = new Variable("distributionVariable",states);
+        List<Variable> v= new ArrayList<Variable>();
+        v.add(distributionVariable);
+        v.addAll(variables.subList(1,variables.size()));
+        this.tableWithEvents = new TransitionTablePotential(v,role);
+    }
 
     //TODO
     public TimeToEventTablePotential(TimeToEventTablePotential potential) {
         super(potential);
-        this.setTablePotential(new TablePotential(potential.getTablePotential()));
+
     }
+
 
 
 
@@ -71,28 +91,104 @@ public class TimeToEventTablePotential extends TransitionTablePotential implemen
         return (variableSuitable && eventSuitable);
     }
 
-    /**
-     * Returns true is this event can happen with the current configuration (stateValue, previous event)
-     * @return true if the event is possible with the stateValue and the event w
-     */
-    public boolean eventPossible(Variable stateVariable, State stateValue, String event){
-        Finding findingState = new Finding(stateVariable,stateValue);
-        Finding findingEvent;
+
+    public void changeDistribution(String distributionName){
+        this.distributionName= distributionName;
         try {
-            findingEvent = new Finding(this.eventAsStates, eventAsStates.getState(event));
-        } catch (InvalidStateException e) {
+            distribution =  ProbDensFunctionManager.getUniqueInstance().getProbDensFunctionClass(distributionName).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-
-        return true;
+        String[] s=  ProbDensFunctionManager.getUniqueInstance().getParameters(distributionName);
+        State[] states = new State[s.length];
+        for (int i=0; i< s.length;i++){
+            states[i] = new State(s[i]);
+        }
+        distributionVariable = new Variable("distributionVariable",states);
+        List<Variable> v= new ArrayList<Variable>();
+        v.add(distributionVariable);
+        v.addAll(variables.subList(1,variables.size()));
+        this.tableWithEvents = new TransitionTablePotential(v,role);
     }
+
 
     @Override
     public double getTimeToEvent(Variable stateVariable, State stateValue, String event) {
-        Finding finding = new Finding(stateVariable,stateValue);
-
         return 0;
     }
+
+
+    /**
+     * @param configuration
+     * @return
+     */
+    @Override
+    public double getTimeToEvent(Configuration configuration) {
+         return 0;
+    }
+
+    @Override
+    public double getTimeToEvent(EvidenceCase ev) {
+        return 0;
+    }
+
+    public TableWithEventsPotential getTableWithEvents() {
+        return tableWithEvents;
+    }
+
+    public void setTableWithEvents(TransitionTablePotential tableWithEvents) {
+        this.tableWithEvents = tableWithEvents;
+    }
+
+    @Override
+    public void addImpossibleConfiguration(EvidenceCase configuration) {
+            addImpossibleConfiguration(new Configuration(configuration));
+    }
+
+    @Override
+    public void addImpossibleConfiguration(Configuration configuration) {
+        tableWithEvents.addImpossibleConfiguration(configuration);
+    }
+
+    @Override
+    public void removeImpossibleConfiguration(EvidenceCase configuration) {
+        removeImpossibleConfiguration(new Configuration(configuration));
+
+    }
+
+    @Override
+    public void removeImpossibleConfiguration(Configuration configuration) {
+        tableWithEvents.removeImpossibleConfiguration(configuration);
+    }
+
+    @Override
+    public boolean hasImpossibleConfiguration() {
+        return  tableWithEvents.hasImpossibleConfiguration();
+    }
+
+    @Override
+    public boolean isImpossibleConfiguration(EvidenceCase configuration) {
+        return tableWithEvents.isImpossibleConfiguration(configuration);
+    }
+
+    @Override
+    public boolean isImpossibleConfiguration(Configuration configuration) {
+        return tableWithEvents.isImpossibleConfiguration(configuration);
+    }
+
+    @Override
+    public ArrayList<Configuration> getImpossibleConfigurations() {
+        return tableWithEvents.getImpossibleConfigurations();
+    }
+
+    @Override
+    public void setImpossibleConfigurations(ArrayList<Configuration> impossibleConfigurations) {
+        tableWithEvents.setImpossibleConfigurations(impossibleConfigurations);
+    }
+
+
 
 
     //TODO
@@ -127,9 +223,15 @@ public class TimeToEventTablePotential extends TransitionTablePotential implemen
 
     //TODO
     @Override public void scalePotential(double scale) {
-        this.getTablePotential().scalePotential(scale);
     }
 
 
+    public String getDistributionName() {
+        return distributionName;
+    }
+
+    public void setDistributionName(String distributionName) {
+        this.distributionName = distributionName;
+    }
 }
 

37fcda5aa6d1aaac9c5abb5b32deb5927378a957
carmenyago <carmen.yago@gmail.com>
2019-12-16 10:51:12 +0100

    Refactoring

--- src/main/java/org/openmarkov/core/model/network/potential/TimeToEvent.java
+++ src/main/java/org/openmarkov/core/model/network/potential/TimeToEvent.java
@@ -1,11 +1,13 @@
 package org.openmarkov.core.model.network.potential;
 
+import cern.jet.random.engine.RandomEngine;
 import org.openmarkov.core.model.network.EvidenceCase;
 import org.openmarkov.core.model.network.Configuration;
 import org.openmarkov.core.model.network.State;
 import org.openmarkov.core.model.network.Variable;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Interface implemented by Potentials used in DES models to represent and  calculate Time To Event behaviour
@@ -21,9 +23,9 @@ public interface TimeToEvent {
      */
     public double getTimeToEvent(Variable stateVariable, State stateValue, String event);
 
-    public double getTimeToEvent(Configuration configuration);
 
-    public double getTimeToEvent(EvidenceCase ev);
+    public double getTimeToEvent(EvidenceCase ev, Random random);
 
 
+    double getTimeToEvent(Configuration eC, Random random);
 }

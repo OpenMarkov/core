72f2dd6d0aff97229ef3c802fc54b556173dc6e2
carmenyago <carmen.yago@gmail.com>
2020-03-11 15:48:12 +0100

    Class modified to extend EvidenceCase

--- src/main/java/org/openmarkov/core/model/network/Configuration.java
+++ src/main/java/org/openmarkov/core/model/network/Configuration.java
@@ -2,87 +2,85 @@ package org.openmarkov.core.model.network;
 
 import org.openmarkov.core.exception.IncompatibleEvidenceException;
 import org.openmarkov.core.exception.InvalidStateException;
+import org.openmarkov.core.exception.NoFindingException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Configuration of States and Events representing an impossible configuration in a potential used in a DESNet
- * TODO rename
+ * Configuration of States and Events representing an configuration of Findings in a potential used in a DESNet
+ * TODO rename, How describe it?
  * @author cyago
- * @verion 1.0 20190901 01/09/2019
+ * @version 1.0 - 01/09/2019
+ * @version 1.1 - 04/01/2020 - inherits from EvidenceCase due to we need evidence cases for tables - to be changed with the new version of tables. Inheritance it done because Configuration comparation is needed and EvidenceCase does not have an equal method
  */
-
-public class Configuration {
-
-    private ArrayList<Finding> findings;
+public class Configuration extends EvidenceCase{
 
 
-    public Configuration(List<Finding> findings) {
-        this.findings = new ArrayList<>(findings);
-        sort();
-
-    }
 
     /**
-     *
+     * Creates a Configuration object whose Finding objects are in findings
+     * @param findings List of Finding conforming the configuration
      */
-    public Configuration(EvidenceCase configuration){
-        this(configuration.getFindings());
-    }
-
-
-    public Configuration(Finding... findings){
-        this.findings = new ArrayList<Finding>();
-        for (Finding finding: findings){
-            if (finding !=null)
-            this.findings.add(finding);
-        }
-        sort();
-    }
-
-    public Configuration(Configuration configuration) {
-       this(configuration.getFindings());
-    }
-
-
-    private void  sort(){
-        findings.sort((finding1, finding2) ->
-                finding1.getVariable().getName().compareTo(finding2.getVariable().getName())
-        );
-    }
-
-
-    public EvidenceCase convertToEvidenceCase(){
-        EvidenceCase ec = new EvidenceCase();
+    public Configuration(List<Finding> findings) {
+        //Create an empty EvidenceCase
+        super();
+        //Add findings
         for (Finding finding: findings){
             try {
-                ec.addFinding(finding);
-            } catch (InvalidStateException e) {
-                e.printStackTrace();
-            } catch (IncompatibleEvidenceException e) {
+                addFinding(finding);
+            } catch (InvalidStateException | IncompatibleEvidenceException e) {
                 e.printStackTrace();
             }
         }
-        return ec;
+    }
+
+
+
+    /**
+     * Creates a Configuration object with the same Finding objects as configuration
+     * @param configuration object used to create the new Configuration object
+     */
+    public Configuration(Configuration configuration) {
+        this(configuration.getFindings());
+    }
+
+    /**
+     * Creates a Configuration object whose Finding objects are in findings
+     * @param findings Array of Finding with the findings which conform the new Configuration
+     */
+    public Configuration(Finding... findings){
+        this(Arrays.asList(findings));
+    }
+
+    /**
+     * Returns a List of Finding sorted alphabetically by its Variable name
+     * @return a List of Finding sorted alphabetically by its Variable name
+     */
+    private List<Finding> getSortedFindings() {
+        List<Finding> sortedFindings = getFindings();
+        sortedFindings.sort((f1,f2) -> f1.getVariable().getName().compareTo(f2.getVariable().getName()));
+        return sortedFindings;
     }
 
 
     /**
-     * Two configurations are equal if they have the same findings
-     * @param obj
+     * Indicates whether some other object is "equal to" this one. Two Configuration objects are considered equal if they have the same Finding objects.
+     * @param obj  the reference object with which to compare
      * @return
      */
     @Override
     public boolean equals(Object obj) {
         if ((obj == null) || !(obj instanceof Configuration))return false;
-        Configuration objIC =(Configuration) obj;
-        ArrayList<Finding> configurationObjIC = objIC.getFindings();
-        if (findings.size() != configurationObjIC.size()) return false;
-        for (int i = 0; i< findings.size(); i++){
+        Configuration objAsConfiguration =(Configuration) obj;
+        List<Finding> objAsConfigurationSortedFindings = objAsConfiguration.getSortedFindings();
+        List<Finding> thisSortedFindings = this.getSortedFindings();
+        if (thisSortedFindings.size() != objAsConfigurationSortedFindings.size()) return false;
+        for (int i = 0; i< thisSortedFindings.size(); i++){
             if (!
-                    ( (findings.get(i).getVariable().getBaseName().compareTo(configurationObjIC.get(i).getVariable().getBaseName()) ==0)
-                && (findings.get(i).getState().compareTo(configurationObjIC.get(i).getState()) ==0) )
+                    ( (thisSortedFindings.get(i).getVariable().getBaseName().compareTo(objAsConfigurationSortedFindings.get(i).getVariable().getBaseName()) ==0)
+                && (thisSortedFindings.get(i).getState().compareTo(objAsConfigurationSortedFindings.get(i).getState()) ==0) )
                 ) {
                 return false;
             }
@@ -90,26 +88,17 @@ public class Configuration {
         return true;
     }
 
-    public ArrayList<Finding> getFindings() {
-        return findings;
-    }
-
-    public void setFindings(ArrayList<Finding> findings) {
-        this.findings = findings;
-    }
-
     /**
-     * Remove the finding with variable
-     * @param variable the Variable whose Finding is removed
+     * Adds a new finding with the event Variable variable
+     * @param variable to be added to this Configuration
+     * @throws NoFindingException exception thrown when the type of variable is not EVEN
      */
-    public void remove(Variable variable) {
-        findings.removeIf(f -> f.getVariable().getName().equals(variable.getName()) );
-    }
-
-    /**
-     * Adds a Finding to configuration
-     */
-    public void add (Finding f){
-        findings.add(f);
+    public void addFinding(Variable variable) throws NoFindingException{
+        Finding eventFinding =new Finding(variable,0);
+        try {
+            this.addFinding(eventFinding);
+        } catch (InvalidStateException |IncompatibleEvidenceException e) {
+            e.printStackTrace();
+        }
     }
 }

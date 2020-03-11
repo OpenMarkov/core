b8969593e1c6a862b53177c3a7eb8041f62ffc3a
carmenyago <carmen.yago@gmail.com>
2020-03-11 15:59:19 +0100

    javadoc added and the methods isImpossibleConfiguration(List<Finding> findings);hasCompatiblePossibleConfiguration(Finding finding)

--- src/main/java/org/openmarkov/core/model/network/potential/ImpossibleConfiguration.java
+++ src/main/java/org/openmarkov/core/model/network/potential/ImpossibleConfiguration.java
@@ -1,29 +1,76 @@
 package org.openmarkov.core.model.network.potential;
 
-import org.openmarkov.core.model.network.Configuration;
-import org.openmarkov.core.model.network.EvidenceCase;
-import org.openmarkov.core.model.network.State;
-import org.openmarkov.core.model.network.Variable;
+import org.openmarkov.core.exception.IncompatibleEvidenceException;
+import org.openmarkov.core.exception.InvalidStateException;
+import org.openmarkov.core.exception.NoFindingException;
+import org.openmarkov.core.model.network.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Interface implemented by Potentials used in DES models to represent and  calculate Time To Event behaviour
+ * Interface for implementing the management of parent configurations which cannot be possible for a node in a model
+ * TODO Throw exception when configurations does not exist??
+ * @author cyago
+ * @version 1.0 - 2019
  */
 public interface ImpossibleConfiguration {
 
 
-    public void addImpossibleConfiguration(EvidenceCase configuration);
-    public void addImpossibleConfiguration(Configuration configuration);
-    public void removeImpossibleConfiguration(EvidenceCase configuration);
+    /**
+     * Sets configuration as impossible. That is, this configuration cannot exit in the model
+     * @param configuration - the configuration which cannot exist in the model
+     */
+    public void addImpossibleConfiguration(Configuration configuration) ;
+
+    /**
+     * Sets configuration as possible. That is, this configuration can exit in the model.
+     *
+     * @param configuration - the configuration which now may exist in the model
+     */
     public void removeImpossibleConfiguration(Configuration configuration);
 
+    /**
+     * Returns true if the potential has some impossible configuration of parents. That is a configuration which cannot be possible in the model.
+     * @return true if the potential has impossible configurations
+     */
     public boolean hasImpossibleConfiguration();
-    public boolean isImpossibleConfiguration(EvidenceCase configuration);
+
+    /**
+     * Returns true if configuration is possible or not.
+     * @param configuration - the configuration to check
+     * @return true if the configuration is impossible, false otherwise
+     */
     public boolean isImpossibleConfiguration(Configuration configuration);
 
+    /**
+     * Returns true if the list of findings form an impossible configuration
+     * Useful when having events treated internally in a TableWithEvents. Events are joined together in a TableWithEvents whereas treated as different variables in other cases
+     * @param findings - set of findings
+     * @return
+     */
+    public boolean isImpossibleConfiguration(List<Finding> findings);
 
 
+    /**
+     * Returns true if there is a possible configuration with finding
+     * @param finding finding to check a possible configuration with it
+     * @return true if there is a possible configuration with finding
+     * @throws NoFindingException
+     */
+    public boolean hasCompatiblePossibleConfiguration(Finding finding) throws NoFindingException;
+
+
+
+    /**
+     * This method returns an ArrayList with the impossible configurations of the potential. If there is not impossible configurations an empy array is returned
+     * @return an ArrayList with the impossible configurations of the potential.
+     */
     public ArrayList<Configuration> getImpossibleConfigurations();
+
+    /**
+     * Adds an ArrayList of impossible configurations to the potential
+     * @param impossibleConfigurations the ArrayList with the set of impossible configurations
+     */
     public void setImpossibleConfigurations(ArrayList<Configuration> impossibleConfigurations);
 }

de2d7aff2d56b0d83ad8de175dc0560d59ab6837
carmenyago <carmen.yago@gmail.com>
2022-08-20 12:43:13 +0200

    ExactFunctionNu:adapted to nuisance variance by inheriting from ExactFunctionDES in order to be working with random numbers instead of Random or RandomStream (related to change in sampleConditionedVariable signature )

--- src/main/java/org/openmarkov/core/model/network/modelUncertainty/ParametrizedFunction/ExactFunctionNu.java
+++ src/main/java/org/openmarkov/core/model/network/modelUncertainty/ParametrizedFunction/ExactFunctionNu.java
@@ -7,16 +7,16 @@
 
 package org.openmarkov.core.model.network.modelUncertainty.ParametrizedFunction;
 
-import org.openmarkov.core.model.network.modelUncertainty.ExactFunction;
-import org.openmarkov.core.model.network.modelUncertainty.ProbDensFunction;
+import org.openmarkov.core.model.network.modelUncertainty.ExactFunctionDES;
 /**
  * Parametrization of the Exact distribution which nu as a parameter
  * @author cyago
- * @version 1 20/10/2020 - only implemented the methods currently needed
+ * @version 1 20/08/2022 - only implemented the methods currently needed
+ * @version 2 20/08/2022 -  adapting to nuisance variance by inheriting from ProbDensFunctionWithKnownInverseCDF in order to be working with random numbers instead of Random or RandomStream (related to change in signature)
  */
 
 @ParametrizedFunctionType(distributionName = "Exact", parametrizationName = "Nu", parameters = { "nu"}, isValidForTTE = true)
-public class ExactFunctionNu extends ExactFunction {
+public class ExactFunctionNu extends ExactFunctionDES {
 
 	/**
 	 * Default constructor

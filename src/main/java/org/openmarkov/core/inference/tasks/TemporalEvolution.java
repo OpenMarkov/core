d275088b34d5c62ab506e111fb48311ab9361764
carmenyago <carmen.yago@gmail.com>
2022-11-04 13:05:35 +0100

    Added method TemporalEvolution#getTemporalEvolutionWithDiscount for providing discounted utility results. Previously getTemporalEvolution provided such results and non discounted results where not given.

--- src/main/java/org/openmarkov/core/inference/tasks/TemporalEvolution.java
+++ src/main/java/org/openmarkov/core/inference/tasks/TemporalEvolution.java
@@ -19,11 +19,27 @@ import java.util.HashMap;
 /**
  * @author jperez-martin
  * @author artasom
+ * @version 1.1 cmyago 03/11/2022; added method getTemporalEvolutionWithDiscount
  */
 public interface TemporalEvolution extends Task {
 
 	HashMap<Variable, TablePotential> getTemporalEvolution()
 			throws IncompatibleEvidenceException, UnexpectedInferenceException, NotEvaluableNetworkException;
+	//cmyago 04/11/2022
+	/**
+	 * Returns the temporal evolution of a node or a set of utility nodes where discounting is applied.
+	 * If there is no discounting non-discounted temporal evolution is returned
+	 *
+	 * @return temporal evolution of a node or a set of utility nodes where discounting is applied
+	 * @throws IncompatibleEvidenceException
+	 * @throws UnexpectedInferenceException
+	 * @throws NotEvaluableNetworkException
+	 */
+	default HashMap<Variable, TablePotential> getTemporalEvolutionWithDiscount()
+			throws IncompatibleEvidenceException, UnexpectedInferenceException, NotEvaluableNetworkException {
+		return getTemporalEvolution();
+	}
+//cmyago end 04/11/2022
 
 	void setDecisionVariable(Variable decisionSelected);
 

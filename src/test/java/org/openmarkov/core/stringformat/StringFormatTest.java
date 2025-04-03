19831ec7a13c4a9fe8ac2ee1d337e761e1f44c23
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-04-03 20:53:56 +0200

    [Test] Adapted tests to the new resource system. [Refactor, Test] Moved test-only method of LocalizeResourcesProvider to its test class in LocalizeXMLCompare.

--- src/test/java/org/openmarkov/core/stringformat/StringFormatTest.java
+++ src/test/java/org/openmarkov/core/stringformat/StringFormatTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
+import java.util.Locale;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,14 +18,17 @@ class StringFormatTest {
     private static final String TEST_PATTERN = "The {NetName} net created at {CreationDate, date, short} is not a " +
             "{DesiredNetType} network, and this functionality is only available for {DesiredNetType}";
     
-    private static final String EXPECTED = "The MyNet net created at 03/06/2023 is not a " +
+    private static final String EXPECTED = "The MyNet net created at 3/6/23 is not a " +
             "Bayesian network, and this functionality is only available for Bayesian";
+    
+    private static final Locale MESSAGE_LOCALE = new Locale("es");
     
     /**
      * Tests the generated String is the same as {@link StringFormatTest#EXPECTED}.
      */
     @Test
     final void apply() {
+        Locale.setDefault(MESSAGE_LOCALE);
         Map<String, Object> values = Map.of(
                 "NetName", "MyNet",
                 "CreationDate", java.util.Date.from(Instant.parse("2023-06-03T10:15:30.00Z")),

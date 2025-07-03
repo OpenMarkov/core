277ee9a29c2475dc04a80b197553bb987db40f1e
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-07-03 11:50:28 +0200

    [Refactor] Specified nullability of ownerWindow.

--- src/main/java/org/openmarkov/core/localize/LocalizedException.java
+++ src/main/java/org/openmarkov/core/localize/LocalizedException.java
@@ -8,6 +8,7 @@
 package org.openmarkov.core.localize;
 
 import org.apache.logging.log4j.LogManager;
+import org.jetbrains.annotations.Nullable;
 import org.openmarkov.core.exception.OpenMarkovException;
 
 import javax.swing.*;
@@ -15,50 +16,50 @@ import java.awt.*;
 import java.util.MissingFormatArgumentException;
 
 public class LocalizedException {
-
-	private Exception openMarkovException;
-	private Window ownerWindow;
-	private String localizedMessage;
-	private String localizedTitle;
-
-	public LocalizedException (Exception openMarkovException, Window ownerWindow) {
-		this.openMarkovException = openMarkovException;
-		this.ownerWindow = ownerWindow;
-		this.localizedTitle = StringDatabase.getUniqueInstance().getString("GenericException.title");
-		this.localizedMessage = StringDatabase.getUniqueInstance().getString("GenericException.message");
-
-		if (openMarkovException instanceof OpenMarkovException) {
-
-			String token = ((OpenMarkovException) openMarkovException).getToken();
-			String newTitle = String.format(StringDatabase.getUniqueInstance().getString(token + ".title"));
-			String formattedString = StringDatabase.getUniqueInstance().getString(token + ".message");
-			String newMessage = formattedString;
-			try {
-				newMessage = String
-						.format(formattedString, ((OpenMarkovException) openMarkovException).getAttributes());
-			} catch (MissingFormatArgumentException exception) {
-				LogManager.getLogger(getClass()).warn("Invalid number of arguments in the formatter");
-			}
+    
+    private Exception openMarkovException;
+    private @Nullable Window ownerWindow;
+    private String localizedMessage;
+    private String localizedTitle;
+    
+    public LocalizedException(Exception openMarkovException, @Nullable Window ownerWindow) {
+        this.openMarkovException = openMarkovException;
+        this.ownerWindow = ownerWindow;
+        this.localizedTitle = StringDatabase.getUniqueInstance().getString("GenericException.title");
+        this.localizedMessage = StringDatabase.getUniqueInstance().getString("GenericException.message");
+        
+        if (openMarkovException instanceof OpenMarkovException) {
+            
+            String token = ((OpenMarkovException) openMarkovException).getToken();
+            String newTitle = String.format(StringDatabase.getUniqueInstance().getString(token + ".title"));
+            String formattedString = StringDatabase.getUniqueInstance().getString(token + ".message");
+            String newMessage = formattedString;
+            try {
+                newMessage = String
+                        .format(formattedString, ((OpenMarkovException) openMarkovException).getAttributes());
+            } catch (MissingFormatArgumentException exception) {
+                LogManager.getLogger(getClass()).warn("Invalid number of arguments in the formatter");
+            }
             this.localizedTitle = newTitle;
             if (newMessage != null) {
-				this.localizedMessage = newMessage;
-			}
-		} else {
-			this.localizedTitle = openMarkovException.getMessage();
-			this.localizedMessage = openMarkovException.getLocalizedMessage();
-		}
-	}
-
-	public LocalizedException (Exception openMarkovException) {
-		this(openMarkovException, null);
-	}
-
-	public void showException(){
-		JOptionPane.showMessageDialog(
-				ownerWindow,
-				localizedMessage,
-				localizedTitle,
-				JOptionPane.ERROR_MESSAGE);
-	}
-
+                this.localizedMessage = newMessage;
+            }
+        } else {
+            this.localizedTitle = openMarkovException.getMessage();
+            this.localizedMessage = openMarkovException.getLocalizedMessage();
+        }
+    }
+    
+    public LocalizedException(Exception openMarkovException) {
+        this(openMarkovException, null);
+    }
+    
+    public void showException() {
+        JOptionPane.showMessageDialog(
+                ownerWindow,
+                localizedMessage,
+                localizedTitle,
+                JOptionPane.ERROR_MESSAGE);
+    }
+    
 }

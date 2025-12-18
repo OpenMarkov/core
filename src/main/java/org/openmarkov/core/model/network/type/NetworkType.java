4009a1b020b87f3fde726f7ae552d7cb26366762
manuel <marias@dia.uned.es>
2025-12-18 10:43:02 +0100

    Remove the sealed modifier from IncompatibleEvidenceException, as it leads to compilation issues. Moreover, this modifier is intended for classes that must not be extended by other classes, which cannot be guaranteed in the future.

--- src/main/java/org/openmarkov/core/model/network/type/NetworkType.java
+++ src/main/java/org/openmarkov/core/model/network/type/NetworkType.java
@@ -14,8 +14,7 @@ import org.openmarkov.core.model.network.constraint.PNConstraint;
 
 import java.util.HashMap;
 
-public abstract sealed class NetworkType implements ClassLocalizable
-        permits BayesianNetworkType, DecisionAnalysisNetworkType, DynamicBayesianNetwork, DynamicLimidType, InfluenceDiagramType, LIMIDType, MDPType, MIDType, MarkovNetworkType, POMDPType, TuningNetworkType {
+public abstract class NetworkType implements ClassLocalizable {
 	protected HashMap<Class<? extends PNConstraint>, ConstraintBehavior> constraints;
 
 	public NetworkType() {

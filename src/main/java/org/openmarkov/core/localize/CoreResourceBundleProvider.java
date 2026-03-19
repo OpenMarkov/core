63466b693879cea72d561d2f18e58f9248706da7
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2026-03-19 01:00:05 +0100

    [Refactor] This commit serves to test whether Jenkins can or cannot execute GUI tests.

--- src/main/java/org/openmarkov/core/localize/CoreResourceBundleProvider.java
+++ src/main/java/org/openmarkov/core/localize/CoreResourceBundleProvider.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.openmarkov.annotation_processing.localization_bindings.BindLocalizations;
 import org.openmarkov.core.localize.spi.LocalizeResourcesProvider;
 
-@BindLocalizations(filePath = "core/localize/CoreExceptions_en.xml", fileIsDirectoryChild = true)
+//@BindLocalizations(filePath = "core/localize/CoreExceptions_en.xml", fileIsDirectoryChild = true)
 public class CoreResourceBundleProvider implements LocalizeResourcesProvider {
     
     @Override
